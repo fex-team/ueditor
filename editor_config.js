db02352f8ffd1344d068d1da7b846ba8eccdf967
许恒 <xuheng@baidu.com>
2012-12-03 11:45:40 +0800

    [add]by xuheng

--- editor_config.js
+++ editor_config.js
@@ -107,8 +107,8 @@
 
         //主题配置项,默认是default。有需要的话也可以使用如下这样的方式来自动多主题切换，当然，前提条件是themes文件夹下存在对应的主题文件：
         //现有如下皮肤:default,modern,gorgeous
-        //,theme:'default'
-        //,themePath:URL +"themes/"
+        ,theme:'gorgeous'
+        ,themePath:URL +"themes/"
 
         //若实例化编辑器的页面手动修改的domain，此处需要设置为true
         //,customDomain:false
