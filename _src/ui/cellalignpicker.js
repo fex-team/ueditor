b9e87365e643292ea6453b1bae3227305153fa8b
许恒 <xuheng@baidu.com>
2012-12-13 19:35:35 +0800

    [add]by xuheng 添加单元格对齐方式

--- _src/ui/cellalignpicker.js
+++ _src/ui/cellalignpicker.js
@@ -17,7 +17,7 @@
             var items=this.items;
             return '<div id="##" class="edui-aligncellpicker %%">' +
                 '<div class="edui-aligncellpicker-body">' +
-                '<table onmouseover="$$._onMouseOver(event);"  onclick="$$._onClick(event);">' +
+                '<table onclick="$$._onClick(event);">' +
                 '<tr><td index="0">'+items[0].label+'</td><td index="1">'+items[1].label+'</td><td index="2">'+items[2].label+'</td></tr>' +
                 '<tr><td index="3">'+items[3].label+'</td><td index="4">'+items[4].label+'</td><td index="5">'+items[5].label+'</td></tr>' +
                 '<tr><td index="6">'+items[6].label+'</td><td index="7">'+items[7].label+'</td><td index="8">'+items[8].label+'</td></tr>' +
@@ -28,13 +28,8 @@
         _onClick: function (evt){
             var target= evt.target || evt.srcElement;
             if(target.tagName.toLowerCase()=="td"){
-                Popup.postHide();
-            }
-        },
-        _onMouseOver:function(evt){
-            var target= evt.target || evt.srcElement;
-            if(target.tagName.toLowerCase()=="td"){
                 this.items[target.getAttribute("index")].onclick();
+                Popup.postHide();
             }
         },
         _UIBase_render:UIBase.prototype.render
