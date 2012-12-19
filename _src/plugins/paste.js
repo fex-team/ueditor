163758929c0e9c4786b1117d966ed8b5402165ad
campaign <zhanyi1022@gmail.com>
2012-12-19 19:29:14 +0800

    by zhanyi

--- _src/plugins/paste.js
+++ _src/plugins/paste.js
@@ -20,7 +20,7 @@
 
         var range = this.selection.getRange(),
             bk = range.createBookmark(),
-            //创建剪贴的容器div
+        //创建剪贴的容器div
             pastebin = doc.createElement( 'div' );
 
         pastebin.id = 'baidu_pastebin';
@@ -39,9 +39,9 @@
         range.selectNodeContents( pastebin ).select( true );
 
         setTimeout( function() {
-            
+
             if (browser.webkit) {
-                
+
                 for(var i=0,pastebins = doc.querySelectorAll('#baidu_pastebin'),pi;pi=pastebins[i++];){
                     if(domUtils.isEmptyNode(pi)){
                         domUtils.remove(pi);
@@ -54,7 +54,7 @@
 
             }
 
-			try{
+            try{
                 pastebin.parentNode.removeChild(pastebin);
             }catch(e){}
 
@@ -80,130 +80,221 @@
             },
             notNeedUndo : 1
         };
-
+        var txtContent,htmlContent;
         function filter(div){
-            
+
             var html;
             if ( div.firstChild ) {
-                    //去掉cut中添加的边界值
-                    var nodes = domUtils.getElementsByTagName(div,'span');
-                    for(var i=0,ni;ni=nodes[i++];){
-                        if(ni.id == '_baidu_cut_start' || ni.id == '_baidu_cut_end'){
-                            domUtils.remove(ni);
+                //去掉cut中添加的边界值
+                var nodes = domUtils.getElementsByTagName(div,'span');
+                for(var i=0,ni;ni=nodes[i++];){
+                    if(ni.id == '_baidu_cut_start' || ni.id == '_baidu_cut_end'){
+                        domUtils.remove(ni);
+                    }
+                }
+
+                if(browser.webkit){
+
+                    var brs = div.querySelectorAll('div br');
+                    for(var i=0,bi;bi=brs[i++];){
+                        var pN = bi.parentNode;
+                        if(pN.tagName == 'DIV' && pN.childNodes.length ==1){
+                            pN.innerHTML = '<p><br/></p>';
+
+                            domUtils.remove(pN);
                         }
                     }
-
-                    if(browser.webkit){
-
-                        var brs = div.querySelectorAll('div br');
-                        for(var i=0,bi;bi=brs[i++];){
-                            var pN = bi.parentNode;
-                            if(pN.tagName == 'DIV' && pN.childNodes.length ==1){
-                                pN.innerHTML = '<p><br/></p>';
-                                
-                                domUtils.remove(pN);
-                            }
+                    var divs = div.querySelectorAll('#baidu_pastebin');
+                    for(var i=0,di;di=divs[i++];){
+                        var tmpP = me.document.createElement('p');
+                        di.parentNode.insertBefore(tmpP,di);
+                        while(di.firstChild){
+                            tmpP.appendChild(di.firstChild);
                         }
-                        var divs = div.querySelectorAll('#baidu_pastebin');
-                        for(var i=0,di;di=divs[i++];){
-                            var tmpP = me.document.createElement('p');
-                            di.parentNode.insertBefore(tmpP,di);
-                            while(di.firstChild){
-                                tmpP.appendChild(di.firstChild);
-                            }
-                            domUtils.remove(di);
-                        }
+                        domUtils.remove(di);
+                    }
 
 
 
-                        var metas = div.querySelectorAll('meta');
-                        for(var i=0,ci;ci=metas[i++];){
+                    var metas = div.querySelectorAll('meta');
+                    for(var i=0,ci;ci=metas[i++];){
+                        domUtils.remove(ci);
+                    }
+
+                    var brs = div.querySelectorAll('br');
+                    for(i=0;ci=brs[i++];){
+                        if(/^apple-/.test(ci)){
                             domUtils.remove(ci);
                         }
-
-                        var brs = div.querySelectorAll('br');
-                        for(i=0;ci=brs[i++];){
-                            if(/^apple-/.test(ci)){
-                                domUtils.remove(ci);
-                            }
-                        }
-
                     }
-                    if(browser.gecko){
-                        var dirtyNodes = div.querySelectorAll('[_moz_dirty]');
-                        for(i=0;ci=dirtyNodes[i++];){
-                            ci.removeAttribute( '_moz_dirty' );
-                        }
-                    }
-                    if(!browser.ie ){
-                        var spans = div.querySelectorAll('span.Apple-style-span');
-                        for(var i=0,ci;ci=spans[i++];){
-                            domUtils.remove(ci,true);
-                        }
-                    }
-
-
-                    html = div.innerHTML;
-
-                    var f = me.serialize;
-                    if(f){
-                        //如果过滤出现问题，捕获它，直接插入内容，避免出现错误导致粘贴整个失败
-                        try{
-                            var node =  f.transformInput(
-                                        f.parseHTML(
-                                            //todo: 暂时不走dtd的过滤
-                                            f.word(html)//, true
-                                        ),word_img_flag
-                                    );
-                            //trace:924
-                            //纯文本模式也要保留段落
-                            node = f.filter(node,pasteplain ? {
-                                whiteList: {
-                                    'p': {'br':1,'BR':1,$:{}},
-                                    'br':{'$':{}},
-                                    'div':{'br':1,'BR':1,'$':{}},
-                                    'li':{'$':{}},
-                                    'tr':{'td':1,'$':{}},
-                                    'td':{'$':{}}
-
-                                },
-                                blackList: {
-                                    'style':1,
-                                    'script':1,
-                                    'object':1
-                                }
-                            } : null, !pasteplain ? modify_num : null);
-
-                            if(browser.webkit){
-                                var length = node.children.length,
-                                    child;
-                                while((child = node.children[length-1]) && child.tag == 'br'){
-                                    node.children.splice(length-1,1);
-                                    length = node.children.length;
-                                }
-                            }
-                            html = f.toHTML(node,pasteplain);
-
-                        }catch(e){}
-
-                    }
-
-
-                    //自定义的处理
-                   html = {'html':html};
-
-                   me.fireEvent('beforepaste',html);
-                    //不用在走过滤了
-                   if(html.html){
-                       me.execCommand( 'insertHtml',html.html,true);
-
-                       me.fireEvent("afterpaste");
-
-                   }
 
                 }
-        }
+                if(browser.gecko){
+                    var dirtyNodes = div.querySelectorAll('[_moz_dirty]');
+                    for(i=0;ci=dirtyNodes[i++];){
+                        ci.removeAttribute( '_moz_dirty' );
+                    }
+                }
+                if(!browser.ie ){
+                    var spans = div.querySelectorAll('span.Apple-style-span');
+                    for(var i=0,ci;ci=spans[i++];){
+                        domUtils.remove(ci,true);
+                    }
+                }
 
+
+                html = div.innerHTML;
+
+                var f = me.serialize;
+                if(f){
+                    //如果过滤出现问题，捕获它，直接插入内容，避免出现错误导致粘贴整个失败
+                    try{
+                        var node =  f.transformInput(
+                            f.parseHTML(
+                                //todo: 暂时不走dtd的过滤
+                                f.word(html)//, true
+                            ),word_img_flag
+                        );
+                        //trace:924
+                        //纯文本模式也要保留段落
+                        node = f.filter(node,pasteplain ? {
+                            whiteList: {
+                                'p': {'br':1,'BR':1,$:{}},
+                                'br':{'$':{}},
+                                'div':{'br':1,'BR':1,'$':{}},
+                                'li':{'$':{}},
+                                'tr':{'td':1,'$':{}},
+                                'td':{'$':{}}
+
+                            },
+                            blackList: {
+                                'style':1,
+                                'script':1,
+                                'object':1
+                            }
+                        } : null, !pasteplain ? modify_num : null);
+
+                        if(browser.webkit){
+                            var length = node.children.length,
+                                child;
+                            while((child = node.children[length-1]) && child.tag == 'br'){
+                                node.children.splice(length-1,1);
+                                length = node.children.length;
+                            }
+                        }
+
+                        html = f.toHTML(node,pasteplain);
+                        txtContent = f.filter(node,{
+                            whiteList: {
+                                'p': {'br':1,'BR':1,$:{}},
+                                'br':{'$':{}},
+                                'div':{'br':1,'BR':1,'$':{},'table':1,'ul':1,'ol':1},
+                                'li':{'$':{}},
+                                'ul':{'li':1,'$':{}},
+                                'ol':{'li':1,'$':{}},
+                                'tr':{'td':1,'$':{}},
+                                'td':{'$':{}},
+                                'table': {'tr':1,'tbody':1,'td':1,'$':{}},
+                                'tbody': {'tr':1,'td':1,'$':{}},
+                                h1:{'$':{}},h2:{'$':{}},h3:{'$':{}},h4:{'$':{}},h5:{'$':{}},h6:{'$':{}}
+                            },
+                            blackList: {
+                                'style':1,
+                                'script':1,
+                                'object':1
+                            }
+                        });
+
+                        txtContent = f.toHTML(txtContent,true)
+
+                    }catch(e){}
+
+                }
+
+                //自定义的处理
+                html = {'html':html};
+
+                me.fireEvent('beforepaste',html);
+                //不用在走过滤了
+                if(html.html){
+                    clearBk();
+                    txtContent = '<span style="display:none;line-height:0px;" id="_ue_paste_id_start">\ufeff</span>'
+                        + txtContent + '<span style="display:none;line-height:0px;" id="_ue_paste_id_end">\ufeff</span>';
+                    htmlContent =  '<span style="display:none;line-height:0px;" id="_ue_paste_id_start">\ufeff</span>'
+                        + html.html + '<span style="display:none;line-height:0px;" id="_ue_paste_id_end">\ufeff</span>';
+                    me.execCommand( 'insertHtml',htmlContent,true);
+                    me.addListener('mouseup keydown',clearBk);
+                    me.fireEvent("afterpaste");
+                }
+
+            }
+        }
+        function clearBk(){
+            var bk,flag = 0;
+            while(bk = me.document.getElementById('_ue_paste_id_start')){
+                flag = 1;
+                domUtils.remove(bk)
+            }
+            while(bk = me.document.getElementById('_ue_paste_id_end')){
+                domUtils.remove(bk)
+            }
+            if(flag){
+                me.removeListener('mouseup keydown',arguments.callee)
+            }
+        }
+        var startAddr,endAddr;
+        me.addListener('beforegetscene',function(){
+
+            var start = me.document.getElementById('_ue_paste_id_start');
+            if(start){
+                startAddr = domUtils.getAddr(start);
+
+            }
+            var end = me.document.getElementById('_ue_paste_id_end');
+            if(end){
+                endAddr = domUtils.getAddr(end);
+
+            }
+            if(start && end){
+                domUtils.remove(start);
+                domUtils.remove(end)
+            }
+        });
+        me.addListener('aftergetscene',function(){
+            var span = domUtils.createElement(me.document,'span',{
+                'style' : "display:none;line-height:0px;",
+                'id'  : "_ue_paste_id_start",
+                'innerHTML' : domUtils.fillChar
+            });
+            if(startAddr){
+                domUtils.setAddr(span,startAddr);
+                startAddr = null;
+            }
+            if(endAddr){
+                span = span.cloneNode(true);
+                span.id = '_ue_paste_id_end';
+                domUtils.setAddr(span,endAddr);
+                endAddr = null;
+            }
+        });
+        me.addListener('pasteTransfer',function(cmd,plainType){
+
+            var range = me.selection.getRange();
+            var start = me.document.getElementById('_ue_paste_id_start');
+            var end = me.document.getElementById('_ue_paste_id_end');
+            if(start && end && txtContent && htmlContent && txtContent != htmlContent){
+
+                range.setStartAfter(start).setEndAfter(end).deleteContents();
+                range.setStartBefore(start).collapse(true);
+                domUtils.remove(start);
+                range.select(true);
+
+                me.__hasEnterExecCommand = true;
+                me.execCommand('inserthtml',plainType ? txtContent : htmlContent,true);
+                me.__hasEnterExecCommand = false;
+                me.addListener('mouseup keydown',clearBk)
+            }
+        });
         me.addListener('ready',function(){
             domUtils.on(me.body,'cut',function(){
 
@@ -211,21 +302,22 @@
                 if(!range.collapsed && me.undoManger){
                     me.undoManger.save();
                 }
-       
+
             });
             //ie下beforepaste在点击右键时也会触发，所以用监控键盘才处理
-                domUtils.on(me.body, browser.ie || browser.opera ? 'keydown' : 'paste',function(e){
+            domUtils.on(me.body, browser.ie || browser.opera ? 'keydown' : 'paste',function(e){
 
-                    if((browser.ie || browser.opera) && (!e.ctrlKey || e.keyCode != '86')){
-                        return;
-                    }
-                    getClipboardData.call( me, function( div ) {
+                if((browser.ie || browser.opera) && (!e.ctrlKey || e.keyCode != '86')){
+                    return;
+                }
 
-                        filter(div);
-                    } );
+                getClipboardData.call( me, function( div ) {
+
+                    filter(div);
+                } );
 
 
-                });
+            });
 
         });
 
