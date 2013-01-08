9c18009e3568935c5c0a27981a90397badd6d579
许恒 <xuheng@baidu.com>
2013-01-08 14:26:08 +0800

    [fixed]by xuheng

--- editor_config.js
+++ editor_config.js
@@ -121,7 +121,7 @@
 
         //,initialContent:'欢迎使用ueditor!'    //初始化编辑器的内容,也可以通过textarea/script给值，看官网例子
 
-        //,initialFrameWidth:1000  //初始化编辑器宽度,默认1000
+        //,initialFrameWidth:100%  //初始化编辑器宽度,100%
         //,initialFrameHeight:320  //初始化编辑器高度,默认320
 
         //,autoClearinitialContent:true //是否自动清除编辑器初始内容，注意：如果focus属性设置为true,这个也为真，那么编辑器一上来就会触发导致初始化的内容看不到了
