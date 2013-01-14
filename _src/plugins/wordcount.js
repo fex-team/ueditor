d746a09fd23eb3f98f8edc86fff62772d546001a
campaign <zhanyi1022@gmail.com>
2013-01-14 17:42:54 +0800

    by zhanyi

--- _src/plugins/wordcount.js
+++ _src/plugins/wordcount.js
@@ -12,32 +12,15 @@
 
 UE.plugins['wordcount'] = function(){
     var me = this;
-    me.setOpt({
-        wordCount:true,
-        wordCountIngoreHtml:true,
-        maximumWords:10000,
-        wordCountMsg: me.options.wordCountMsg||me.getLang("wordCountMsg"),
-        wordOverFlowMsg:me.options.wordOverFlowMsg||me.getLang("wordOverFlowMsg")
+    me.addListener('contentchange',function(){
+        me.fireEvent('wordcount')
     });
-    var opt = me.options,
-        max = opt.maximumWords,
-        msg = opt.wordCountMsg ,
-        errMsg = opt.wordOverFlowMsg;
-    if(!opt.wordCount){
-        return;
-    }
-    me.commands["wordcount"]={
-        queryCommandValue:function(cmd,onlyCount){
-            var length;
-            if(onlyCount){
-                return this.getContentLength(this.options.wordCountIngoreHtml);
-            }
-            length = this.getContentLength(this.options.wordCountIngoreHtml);
-            if(max-length<0){
-                me.fireEvent('wordcountoverflow',length);
-                return errMsg;
-            }
-            return msg.replace("{#leave}",max-length >= 0 ? max-length:0).replace("{#count}",length);
-        }
-    };
+    var timer;
+    me.addListener('keyup',function(){
+        clearTimeout(timer);
+        var me = this;
+        timer = setTimeout(function(){
+            me.fireEvent('wordcount')
+        },200)
+    });
 };
