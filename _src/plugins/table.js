4d01afb6679f50479a28dd9e428e48f37fbc7ce0
taoqili <taoqili@126.com>
2013-01-14 12:55:14 +0800

    预览时去除表格框选样式

--- _src/plugins/table.js
+++ _src/plugins/table.js
@@ -769,7 +769,7 @@ UE.plugins['table'] = function () {
             // 部分浏览器下需要清理
             clearTimeout(timer);
             timer = setTimeout(function () {
-                editor.fireEvent("startDragTable", table, button);
+                editor.fireEvent("tableClicked", table, button);
             }, 300);
         }
 
