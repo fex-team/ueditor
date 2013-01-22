cf7f3b784edb5e442f00dda0aa4a22091a46c9af
campaign <zhanyi1022@gmail.com>
2013-01-23 02:52:47 +0800

    by zhanyi

--- _src/plugins/table.js
+++ _src/plugins/table.js
@@ -25,7 +25,7 @@ UE.plugins['table'] = function () {
 
     var mousedown = false,
     //todo 判断混乱模式
-        needIEHack = browser.ie;
+        needIEHack = true;
 
     me.setOpt({
         'maxColNum':20,
@@ -126,7 +126,7 @@ UE.plugins['table'] = function () {
             if (keyCode == 13) {
 
                 var rng = me.selection.getRange(),
-                    caption = domUtils.findParentByTagName(rng.startContainer, 'caption');
+                    caption = domUtils.findParentByTagName(rng.startContainer, 'caption',true);
                 if (caption) {
                     var table = domUtils.findParentByTagName(caption, 'table');
                     if (!rng.collapsed) {
