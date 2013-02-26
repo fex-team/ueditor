c4894cf2733c29889d8269f3fc6faea855d4ba13
许恒 <xuheng@baidu.com>
2013-02-26 16:02:14 +0800

    [fixed]by xuheng

--- _src/plugins/preview.js
+++ _src/plugins/preview.js
@@ -16,8 +16,8 @@ UE.commands['preview'] = {
         d.write('<html><head><script src="'+this.options.UEDITOR_HOME_URL+'uparse.js"></script><script>' +
             "setTimeout(function(){uParse('div',{" +
             "    'highlightJsUrl':'"+this.options.UEDITOR_HOME_URL+"third-party/SyntaxHighlighter/shCore.js'," +
-                "    'highlightCssUrl':'"+this.options.UEDITOR_HOME_URL+"third-party/SyntaxHighlighter/shCoreDefault.css'" +
-//            "    'formulaUrl':'"+this.options.UEDITOR_HOME_URL+"third-party/MathJax/MathJax.js?config=TeX-AMS_HTML'," +
+                "    'highlightCssUrl':'"+this.options.UEDITOR_HOME_URL+"third-party/SyntaxHighlighter/shCoreDefault.css'," +
+            "    'formulaUrl':'"+this.options.UEDITOR_HOME_URL+"third-party/MathJax/MathJax.js?config=TeX-AMS_HTML'" +
             "})},300)" +
             '</script></head><body><div>'+this.getContent(null,null,true)+'</div></body></html>');
         d.close();
