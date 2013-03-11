33bfaecae25f4799215890c3a6353093fb5f0b47
许恒 <xuheng@baidu.com>
2013-03-11 14:47:05 +0800

    [fixed] byxuheng

--- editor_config.js
+++ editor_config.js
@@ -293,9 +293,9 @@
 
         //formula
         // 数学公式时需要加载的第三方插件的路径
-        // ,formulaCssUrl:URL + "third-party/mathquill-0.9.1/mathquill.css"
-        // ,jqueryUrl:URL + "third-party/mathquill-0.9.1/jquery-1.8.3.min.js"
-        // ,formulaJsUrl:URL + "third-party/mathquill-0.9.1/mathquill.min.js"
+        // ,formulaCssUrl:URL + "third-party/mathquill/mathquill.css"
+        // ,jqueryUrl:URL + "third-party/mathquill/jquery-1.8.3.min.js"
+        // ,formulaJsUrl:URL + "third-party/mathquill/mathquill.min.js"
 
         //tab
         //点击tab键时移动的距离,tabSize倍数，tabNode什么字符做为单位
