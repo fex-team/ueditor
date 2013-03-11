2ef36f30ae03edadd1c1e913074034f738b12809
campaign <zhanyi1022@gmail.com>
2013-03-11 18:08:48 +0800

    by zhanyi

--- _src/core/node.js
+++ _src/core/node.js
@@ -142,7 +142,23 @@
             }
         }
     }
+    function nodeTraversal(root,fn){
+        if(root.children && root.children.length){
+            for(var i= 0,ci;ci=root.children[i];){
+                nodeTraversal(ci,fn);
+                //ci被替换的情况，这里就不再走 fn了
+                if(ci.parentNode ){
+                    if(ci.children){
+                        fn(ci)
+                    }
+                    if(ci.parentNode) i++
+                }
+            }
+        }else{
+            fn(root)
+        }
 
+    }
     uNode.prototype = {
         toHtml:function (formatter) {
             var arr = [];
@@ -390,6 +406,12 @@
                 exec(name, val)
             }
             this.setAttr('style', cssStyle)
+        },
+        traversal:function(fn){
+            if(this.children && this.children.length){
+                nodeTraversal(this,fn);
+            };
+            return this;
         }
     }
 })();
