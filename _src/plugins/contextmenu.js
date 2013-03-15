1fcf093b1da51eba15dd6195bba7941bb77d79cf
许恒 <xuheng@baidu.com>
2013-03-15 18:37:20 +0800

    [fixed] by xuheng

--- _src/plugins/contextmenu.js
+++ _src/plugins/contextmenu.js
@@ -375,7 +375,15 @@ UE.plugins['contextmenu'] = function () {
                         }
                         this.ui._dialogs['highlightcodeDialog'].open();
                     }
-                }
+                },
+                {
+                    label:"内嵌",
+                    cmdName:'formulainline'
+                },
+                {
+                    label:"显示",
+                    cmdName:'formulablock'
+                },
             ];
     if ( !items.length ) {
         return;
