edae3b8dacb273b644a00dfbc0eb801e1159bb6a
campaign <zhanyi1022@gmail.com>
2013-03-18 23:30:52 +0800

    by zhanyi

--- _src/plugins/defaultfilter.js
+++ _src/plugins/defaultfilter.js
@@ -1,13 +1,6 @@
 ///import core
-///commands 提供黑白名单功能
-///commandsName  filterNode
-///commandsTitle  filterNode
-/**
- * 提供黑白名单功能
- * @function
- * @name baidu.editor.execCommands
- * @param {uNode} node
- */
+///plugin 编辑器默认的过滤转换机制
+
 UE.plugins['defaultfilter'] = function (){
     var me = this;
     //默认的过滤处理
@@ -42,8 +35,11 @@ UE.plugins['defaultfilter'] = function (){
                     break;
                 case 'span':
                     if(browser.webkit && (val = node.getStyle('white-space'))){
-                        if(val == 'nowrap'){
-                            node.setStyle('white-space','')
+                        if(/nowrap|normal/.test(val)){
+                            node.setStyle('white-space','');
+                            if(utils.isEmptyObject(node.attrs)){
+                                node.parentNode.removeChild(node,true)
+                            }
                         }
                     }
                     break;
@@ -65,6 +61,82 @@ UE.plugins['defaultfilter'] = function (){
                 }
             })
         }
+        //进行默认的处理
+        root.traversal(function(node){
+            if(node.type == 'element'){
+                switch(node.tagName){
+                    case 'p':
+                        var cssStyle = node.getAttr('style');
+                        if(cssStyle){
+                            cssStyle = cssStyle.replace(/(margin|padding)[^;]+/,'');
+                            node.setAttr('style',cssStyle)
+
+                        }
+                        if(!node.firstChild()){
+                            node.innerHTML(UE.browser.ie ? '&nbsp;' : '<br>')
+                        }
+                        break;
+                    case 'div':
+                        var tmpNode, p = UE.uNode.createElement('p');
+                        while (tmpNode = node.firstChild()) {
+                            if (tmpNode.type == 'text' || !UE.dom.dtd.$block[tmpNode.tagName]) {
+                                p.appendChild(tmpNode);
+                            } else {
+                                if (p.firstChild()) {
+                                    node.parentNode.insertBefore(p, node);
+                                    p = UE.uNode.createElement('p');
+                                } else {
+                                    node.parentNode.insertBefore(tmpNode, node);
+                                }
+                            }
+                        }
+                        if (p.firstChild()) {
+                            node.parentNode.insertBefore(p, node);
+                        }
+                        node.parentNode.removeChild(node);
+                        break;
+                    case 'dl':
+                        node.tagName = 'ul';
+                        break;
+                    case 'dt':
+                    case 'dd':
+                        node.tagName = 'li';
+                        break;
+                    case 'li':
+                        var className = node.getAttr('class');
+                        if (!className || !/list\-/.test(className)) {
+                            node.setAttr()
+                        }
+                        var tmpNodes = node.getNodesByTagName('ol ul');
+                        UE.utils.each(tmpNodes,function(n){
+                            node.parentNode.insertAfter(n,node);
+
+                        });
+                        break;
+                    case 'table':
+                        UE.utils.each(node.getNodesByTagName('table'), function (t) {
+                            UE.utils.each(t.getNodesByTagName('tr'), function (tr) {
+                                var p = UE.uNode.createElement('p'), child, html = [];
+                                while (child = tr.firstChild()) {
+                                    html.push(child.innerHTML());
+                                    tr.removeChild(child);
+                                }
+                                p.innerHTML(html.join('&nbsp;&nbsp;'));
+                                t.parentNode.insertBefore(p, t);
+                            })
+                            t.parentNode.removeChild(t)
+                        });
+                        var val = node.getAttr('width');
+                        node.setAttr();
+                        if (val) {
+                            node.setAttr('width', val);
+                        }
+
+                }
+
+            }
+        })
+
     });
 
     //从编辑器出去的内容处理
