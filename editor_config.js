763584f0749bc0a1498bbd1ce2af649ae478bb43
campaign <zhanyi1022@gmail.com>
2013-04-12 17:12:37 +0800

    by zhanyi

--- editor_config.js
+++ editor_config.js
@@ -158,12 +158,12 @@
                 'print', 'preview', 'searchreplace', 'help']
         ]
         //当鼠标放在工具栏上时显示的tooltip提示,留空支持自动多语言配置，否则以配置值为准
-        ,labelMap:{
-            'anchor':'', 'undo':''
-        }
+//        ,labelMap:{
+//            'anchor':'', 'undo':''
+//        }
         //webAppKey
         //百度应用的APIkey，每个站长必须首先去百度官网注册一个key后方能正常使用app功能
-        ,webAppKey:""
+        //,webAppKey:""
 
         //语言配置项,默认是zh-cn。有需要的话也可以使用如下这样的方式来自动多语言切换，当然，前提条件是lang文件夹下存在对应的语言文件：
         //lang值也可以通过自动获取 (navigator.language||navigator.browserLanguage ||navigator.userLanguage).toLowerCase()
