564184bb451dd9ec824e321fc76abc34d4cacec2
yancen <dongyancen@baidu.com>
2013-04-23 18:52:10 +0800

    [case] 修改用例：代码结尾加换行

--- _test/plugins/highlightcode.js
+++ _test/plugins/highlightcode.js
@@ -6,39 +6,42 @@
  * To change this template use File | Settings | File Templates.
  */
 module('plugins.highlightcode');
-
+var toolbars_ = [
+    [ 'source','inserthtml','highlightcode']
+];
 /*trace 3290*/
 test('插入代码',function(){
+    var editor = new baidu.editor.Editor({'UEDITOR_HOME_URL':'../../../','autoFloatEnabled':false,'toolbars':toolbars_});
     var div = document.body.appendChild( document.createElement( 'div' ) );
     $( div ).css( 'width', '500px' ).css( 'height', '500px' ).css( 'border', '1px solid #ccc' );
-    te.obj[2].render(div);
-    var range = new baidu.editor.dom.Range( te.obj[2].document );
+    editor.render(div);
+    var range = new baidu.editor.dom.Range( editor.document );
     stop();
-    te.obj[2].ready(function(){
+    editor.ready(function(){
         var br = baidu.editor.browser.ie ? '&nbsp;' : '<br />';
-        te.obj[2].setContent('<p>' + br + '</p>');
-        range.setStart(te.obj[2].body.firstChild,0).collapse(1).select();
+        editor.setContent('<p>' + br + '</p>');
+        range.setStart(editor.body.firstChild,0).collapse(1).select();
         setTimeout(function(){
-            te.obj[2].execCommand('highlightcode','<?php echo "Hello World"; ?>','php');
-            ua.manualDeleteFillData(te.obj[2].body);
-            equal( te.obj[2].getContent().substring(0, 81),'<pre class=\"brush: php;toolbar:false;\" >&lt;?php echo "Hello World"; ?&gt; </pre>','代码高亮');
-            var tds = te.obj[2].body.firstChild.getElementsByTagName('td');
+            editor.execCommand('highlightcode','<?php echo "Hello World"; ?>','php');
+            var s = '<pre class=\"brush: php;toolbar:false;\" >&lt;?php echo "Hello World"; ?&gt;\n</pre>';
+            equal( editor.getContent().substring(0,81),s,'代码高亮');
+            var tds = editor.body.firstChild.getElementsByTagName('td');
             range.selectNode(tds[0]).select();
-            equal(te.obj[2].queryCommandState('highlightcode'),1,'插入代码高亮');
-            te.obj[2].execCommand('highlightcode','<?php echo "Hello World"; echo "Hello World"; echo "Hello World"; echo "Hello World"; echo "Hello World"; ?>','php');
+            equal(editor.queryCommandState('highlightcode'),1,'插入代码高亮');
+            editor.execCommand('highlightcode','<?php echo "Hello World"; echo "Hello World"; echo "Hello World"; echo "Hello World"; echo "Hello World"; ?>','php');
             stop();
             setTimeout(function(){
-                equal( te.obj[2].getContent().substring(0, 161),'<pre class=\"brush: php;toolbar:false;\ ">&lt;?php echo \"Hello World\"; echo \"Hello World\"; echo \"Hello World\"; echo \"Hello World\"; echo \"Hello World\"; ?&gt; </pre>','代码修改');
-                te.obj[2].fireEvent('fullscreenchanged');
+                equal( editor.getContent().substring(0,161),'<pre class="brush: php;toolbar:false;" >&lt;?php echo "Hello World"; echo "Hello World"; echo "Hello World"; echo "Hello World"; echo "Hello World"; ?&gt;\n</pre>','代码修改');
+                editor.fireEvent('fullscreenchanged');
                 var html = [];
-                te.obj[2].fireEvent('getAllHtml',html);
-                ok(html[0].indexOf('SyntaxHighlighter')!=-1,'加载SyntaxHighlighter');
-                ua.manualDeleteFillData(te.obj[2].body);
-                tds = te.obj[2].body.getElementsByTagName('td');
+                editor.fireEvent('getAllHtml',html);
+                ok(html[1].indexOf('SyntaxHighlighter')!=-1,'加载SyntaxHighlighter');
+                ua.manualDeleteFillData(editor.body);
+                tds = editor.body.getElementsByTagName('td');
                 range.selectNode(tds[0]).select();
-                te.obj[2].execCommand('highlightcode');
+                editor.execCommand('highlightcode');
                 var br = ua.browser.ie?'&nbsp;':'<br>'
-                equal(ua.getChildHTML(te.obj[2].body),'<p>'+br+'</p>','去掉代码高亮');
+                equal(ua.getChildHTML(editor.body),'<p>'+br+'</p>','去掉代码高亮');
                 div.parentNode.removeChild(div);
                 start();
             },50);
@@ -72,18 +75,19 @@ test('插入代码',function(){
 
 /*trace 2472*/
 test('插入两个字符',function(){
+    var editor = new baidu.editor.Editor({'UEDITOR_HOME_URL':'../../../','autoFloatEnabled':false,'toolbars':toolbars_});
     var div = document.body.appendChild( document.createElement( 'div' ) );
     $( div ).css( 'width', '500px' ).css( 'height', '500px' ).css( 'border', '1px solid #ccc' );
-    te.obj[2].render(div);
-    var range = new baidu.editor.dom.Range( te.obj[2].document );
+    editor.render(div);
+    var range = new baidu.editor.dom.Range( editor.document );
     stop();
         setTimeout(function(){
-            te.obj[2].setContent('<p></p>');
-            range.setStart(te.obj[2].body.firstChild,0).collapse(true).select();
-            te.obj[2].execCommand('highlightcode','aaa','php');
-            ua.manualDeleteFillData(te.obj[2].body);
+            editor.setContent('<p></p>');
+            range.setStart(editor.body.firstChild,0).collapse(true).select();
+            editor.execCommand('highlightcode','aaa','php');
+            ua.manualDeleteFillData(editor.body);
             var br = ua.browser.ie?' ':'<br/>';
-            equal( te.obj[2].getContent().substring(0, 158),'<pre class=\"brush: php;toolbar:false;\" >aaa</pre><p>'+br+'</p>','插入成功');
+            equal( editor.getContent().substring(0, 158),'<pre class=\"brush: php;toolbar:false;\" >aaa\n</pre><p>'+br+'</p>','插入成功');
             div.parentNode.removeChild(div);
             start();
     },50);
@@ -92,23 +96,24 @@ test('插入两个字符',function(){
 /*trace 3138*/
 /*trace 3203*/
 test('插入代码后撤销',function(){
+    var editor = new baidu.editor.Editor({'UEDITOR_HOME_URL':'../../../','autoFloatEnabled':false,'toolbars':toolbars_});
     var div = document.body.appendChild( document.createElement( 'div' ) );
     $( div ).css( 'width', '500px' ).css( 'height', '500px' ).css( 'border', '1px solid #ccc' );
-    te.obj[2].render(div);
-    var range = new baidu.editor.dom.Range( te.obj[2].document );
+    editor.render(div);
+    var range = new baidu.editor.dom.Range( editor.document );
     stop();
-    te.obj[2].ready(function(){
+    editor.ready(function(){
         var br = baidu.editor.browser.ie ? '' : '<br />';
-        te.obj[2].setContent('<p>' + br + '</p>');
-        range.setStart(te.obj[2].body.firstChild,0).collapse(1).select();
+        editor.setContent('<p>' + br + '</p>');
+        range.setStart(editor.body.firstChild,0).collapse(1).select();
         setTimeout(function(){
-            te.obj[2].execCommand('highlightcode','<body><table><tbody><tr><td><br></td></tr></tbody></table></body>','html');
-            ua.manualDeleteFillData(te.obj[2].body);
-            equal( te.obj[2].getContent(),'<pre class="brush: html;toolbar:false;" >&lt;body&gt;&lt;table&gt;&lt;tbody&gt;&lt;tr&gt;&lt;td&gt;&lt;br&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/tbody&gt;&lt;/table&gt;&lt;/body&gt;</pre><p><br/></p>','内容未被改变');
-            equal( te.obj[2].body.style.cursor,'text','检查光标样式');
-            ua.keydown(te.obj[2].body,{'keyCode':90,'ctrlKey':true});
-            equal( te.obj[2].getContent(),'','插入的代码被撤销');
-            equal( te.obj[2].body.style.cursor,'text','检查光标样式');
+            editor.execCommand('highlightcode','<body><table><tbody><tr><td><br></td></tr></tbody></table></body>','html');
+            ua.manualDeleteFillData(editor.body);
+            equal( editor.getContent(),'<pre class="brush: html;toolbar:false;" >&lt;body&gt;&lt;table&gt;&lt;tbody&gt;&lt;tr&gt;&lt;td&gt;&lt;br&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/tbody&gt;&lt;/table&gt;&lt;/body&gt;\n</pre><p><br/></p>','内容未被改变');
+            equal( editor.body.style.cursor,'text','检查光标样式');
+            ua.keydown(editor.body,{'keyCode':90,'ctrlKey':true});
+            equal( editor.getContent(),'','插入的代码被撤销');
+            equal( editor.body.style.cursor,'text','检查光标样式');
             div.parentNode.removeChild(div);
             start();
         },500);
