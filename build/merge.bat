cf460e75d9cf8f9ff21eacf35da603ad4cc95116
campaign <zhanyi1022@gmail.com>
2013-04-23 19:09:47 +0800

    by zhanyi

--- build/merge.bat
+++ build/merge.bat
@@ -1,6 +1,7 @@
 java -jar js.jar ueditor_css_all.js ../themes/default/ ../themes/default/css/ueditor.css
 
 java -jar js.jar ueditor_js_all.js ../ ../ueditor.all.js
-java -jar yuicompressor-2.4.6.jar --nomunge --preserve-semi  --disable-optimizations --charset utf-8 ../ueditor.all.js -o ../ueditor.all.min.js
+java -jar compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js ../ueditor.all.js --js_output_file ../ueditor.all.min.js
+
 
 
