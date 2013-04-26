fd23dfd042dc756a11cdadb8b915aa1bf1568f7d
campaign <zhanyi1022@gmail.com>
2013-04-26 21:53:31 +0800

    by zhanyi

--- _src/plugins/selectall.js
+++ _src/plugins/selectall.js
@@ -28,46 +28,8 @@ UE.plugins['selectall'] = function(){
         },
         notNeedUndo : 1
     };
-    function isBoundaryNode(node,dir){
-        var tmp;
-        while(!domUtils.isBody(node)){
-            tmp = node;
-            node = node.parentNode;
-            if(tmp !== node[dir]){
-                return false;
-            }
-        }
-        return true;
-    }
-    me.addListener('keydown', function(type, evt) {
-        var rng = me.selection.getRange();
 
-        if(!rng.collapsed && !(evt.ctrlKey || evt.metaKey || evt.shiftKey || evt.altKey)){
-            var tmpNode = rng.startContainer;
-            if(domUtils.isFillChar(tmpNode)){
-                rng.setStartBefore(tmpNode)
-            }
-            tmpNode = rng.endContainer;
-            if(domUtils.isFillChar(tmpNode)){
-                rng.setEndAfter(tmpNode)
-            }
-            rng.txtToElmBoundary();
-            if(rng.startOffset == 0){
-                tmpNode = rng.startContainer;
-                if(isBoundaryNode(tmpNode,'firstChild')){
-                    tmpNode = rng.endContainer;
-                    if(rng.endOffset == rng.endContainer.childNodes.length && isBoundaryNode(tmpNode,'lastChild') ){
-                        me.fireEvent('saveScene');
-                        me.body.innerHTML = '<p>'+(browser.ie ? '' : '<br/>')+'</p>';
-                        rng.setStart(me.body.firstChild,0).setCursor(false,true);
-                        me.fireEvent('saveScene');
-                        browser.ie && me._selectionChange();
-                        return;
-                    }
-                }
-            }
-        }
-    });
+
     //快捷键
     me.addshortcutkey({
          "selectAll" : "ctrl+65"
