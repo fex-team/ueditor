231677c134972015c9f52304289c4a5423944515
campaign <zhanyi1022@gmail.com>
2013-05-02 14:58:44 +0800

    by zhanyi

--- makefile.js
+++ makefile.js
@@ -2,7 +2,6 @@ var fs = require('fs'),
     compressor = require('node-minify');
 
 function mergfile(prefix,data,count,content,callback){
-    console.log(prefix + (data[count].replace(/['"]/g,'')))
     fs.readFile(prefix + (data[count].replace(/['"]/g,'')),'utf8',function(err,cont){
         content.push(cont);
         if(++count == data.length){
@@ -18,13 +17,14 @@ fs.readFile('_examples/editor_api.js','utf8',function(err,data){
     var content = [],count = 0;
     mergfile('_src/',data,count,content,function(cont){
         cont = '(function(){\n' + cont.join('\n').replace('_css','css') + '})()';
+        console.log('ueditor.all.js 生成成功');
         fs.writeFile('ueditor.all.js',cont,function(err){
             new compressor.minify({
                 type: 'gcc',
                 fileIn: 'ueditor.all.js',
                 fileOut: 'ueditor.all.min.js',
                 callback: function(err){
-                    console.log(err);
+                    console.log('ueditor.all.min.js 生成成功');
                 }
             });
         });
@@ -38,12 +38,13 @@ fs.readFile('themes/default/_css/ueditor.css','utf8',function(err,data){
     var content = [],count = 0;
     mergfile('themes/default/_css/',data,count,content,function(cont){
         fs.writeFile('themes/default/css/ueditor.css',cont.join('\n'),function(err){
+            console.log('ueditor.css 生成成功');
             new compressor.minify({
                 type: 'sqwish',
                 fileIn: 'themes/default/css/ueditor.css',
                 fileOut: 'themes/default/css/ueditor.min.css',
                 callback: function(err){
-                    console.log(err);
+                    console.log('ueditor.min.css 生成成功');
                 }
             });
         });
