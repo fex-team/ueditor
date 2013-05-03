896a39b3940426d816746db1338018b731e2d817
campaign <zhanyi1022@gmail.com>
2013-05-03 22:36:54 +0800

    by zhanyi

--- makefile.js
+++ makefile.js
@@ -1,65 +1,59 @@
 var fs = require('fs'),
-    compressor = require('node-minify');
+    compressor = require('node-minify'),
+    jsp = require('uglify-js').parser,
+    pro = require('uglify-js').uglify;
 
-function mergfile(prefix,data,count,content,callback){
-    fs.readFile(prefix + (data[count].replace(/['"]/g,'')),'utf8',function(err,cont){
-        content.push(cont);
-        if(++count == data.length){
-            callback(content);
-        }else{
-            mergfile(prefix,data,count,content,callback)
-        }
-    })
-}
 //压缩js
-fs.readFile('_examples/editor_api.js','utf8',function(err,data){
-    data = data.match(/\[([^\]]+)\]/)[1].match(/'[^']+'/g)
-    var content = [],count = 0;
-    mergfile('_src/',data,count,content,function(cont){
-        cont = '(function(){\n' + cont.join('\n').replace('_css','css') + '})()';
-        console.log('ueditor.all.js 生成成功');
-        fs.writeFile('ueditor.all.js',cont,function(err){
-            new compressor.minify({
-                type: 'gcc',
-                fileIn: 'ueditor.all.js',
-                fileOut: 'ueditor.all.min.js',
-                callback: function(err){
-                    if(err && /java/.test(err.toString())){
-                        console.log('没有java环境，使用uglifyjs作为压缩工具');
-                        new compressor.minify({
-                            type: 'uglifyjs',
-                            fileIn: 'ueditor.all.js',
-                            fileOut: 'ueditor.all.min.js',
-                            callback: function(err){
-                                console.log('ueditor.all.min.js 生成成功');
-                            }
-                        });
-                    }else{
-                        console.log('ueditor.all.min.js 生成成功');
-                    }
+var jslist = fs.readFileSync('_examples/editor_api.js','utf8');
+jslist = jslist.match(/\[([^\]]+)\]/)[1].match(/'[^']+'/g);
+var content = [];
+for(var i= 0,ci;ci=jslist[i++];){
+    console.log(ci.replace(/['"]/g,''));
+    content.push(fs.readFileSync('_src/' + ci.replace(/['"]/g,''),'utf8'));
+}
+content = '(function(){\n' + content.join('\n').replace('_css','css') + '})()';
+var ast = jsp.parse(content);
 
+fs.writeFileSync('deploy/ueditor.all.js',pro.gen_code(ast,{beautify:true}));
+console.log('deplay/ueditor.all.js create success');
+new compressor.minify({
+    type: 'gcc',
+    fileIn: 'deploy/ueditor.all.js',
+    fileOut: 'deploy/ueditor.all.min.js',
+    callback: function(err){
+        if(err && /java/.test(err.toString())){
+            console.log('no java environment found,use uglifyjs for compression');
+            new compressor.minify({
+                type: 'uglifyjs',
+                fileIn: 'deploy/ueditor.all.js',
+                fileOut: 'deploy/ueditor.all.min.js',
+                callback: function(err){
+                    console.log('ueditor.all.min.js create success');
                 }
             });
-        });
-    })
+        }else{
+            console.log('ueditor.all.min.js create success');
+        }
+    }
 });
 
 //压缩css
-fs.readFile('themes/default/_css/ueditor.css','utf8',function(err,data){
-    data = data.match(/\"([^\"]+)\"/g);
-
-    var content = [],count = 0;
-    mergfile('themes/default/_css/',data,count,content,function(cont){
-        fs.writeFile('themes/default/css/ueditor.css',cont.join('\n'),function(err){
-            console.log('ueditor.css 生成成功');
-            new compressor.minify({
-                type: 'sqwish',
-                fileIn: 'themes/default/css/ueditor.css',
-                fileOut: 'themes/default/css/ueditor.min.css',
-                callback: function(err){
-                    console.log('ueditor.min.css 生成成功');
-                }
-            });
-        });
-    })
+content = [];
+var csslist = fs.readFileSync('themes/default/_css/ueditor.css','utf8');
+csslist = csslist.match(/\"([^\"]+)\"/g);
+for(var i= 0,ci;ci=csslist[i++];){
+    console.log(ci.replace(/['"]/g,''));
+    content.push(fs.readFileSync('themes/default/_css/' + ci.replace(/['"]/g,''),'utf8'));
+}
+fs.writeFileSync('themes/default/css/ueditor.css',content.join('\n'));
+console.log('ueditor.css create success');
+new compressor.minify({
+    type: 'sqwish',
+    fileIn: 'themes/default/css/ueditor.css',
+    fileOut: 'themes/default/css/ueditor.min.css',
+    callback: function(err){
+        console.log('ueditor.min.css create success');
+    }
 });
+
+
