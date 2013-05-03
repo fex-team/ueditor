4f1341befbb46a9e5f89596799f349873707c004
campaign <zhanyi1022@gmail.com>
2013-05-03 15:36:55 +0800

    by zhanyi

--- _src/plugins/defaultfilter.js
+++ _src/plugins/defaultfilter.js
@@ -68,6 +68,9 @@ UE.plugins['defaultfilter'] = function () {
                         }
                         break;
                     case 'div':
+                        if(node.getAttr('cdata_tag')){
+                            break;
+                        }
                         //针对代码这里不处理插入代码的div
                         val = node.getAttr('class');
                         if(val && /^line number\d+/.test(val)){
