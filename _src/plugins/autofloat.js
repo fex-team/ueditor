678ae969e2a0214b4b599181fc60bafd2fbf77b3
许恒 <xuheng@baidu.com>
2013-05-06 15:27:03 +0800

    [fixed] by xuheng

--- _src/plugins/autofloat.js
+++ _src/plugins/autofloat.js
@@ -76,7 +76,8 @@
 
         function updateFloating(){
             var rect3 = getPosition(me.container);
-            if (rect3.top < 0 && rect3.bottom - toolbarBox.offsetHeight > 0) {
+            var offset=me.options.toolbarTopOffset||0;
+            if (rect3.top < 0 && rect3.bottom - toolbarBox.offsetHeight > offset) {
                 setFloating();
             }else{
                 unsetFloating();
