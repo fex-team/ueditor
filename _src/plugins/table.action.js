2e7e6cec7599defbd52d932ce036f6bab3a2fd30
hancong03 <121371790@qq.com>
2013-05-06 17:57:04 +0800

    表格隔行变色优化

--- _src/plugins/table.action.js
+++ _src/plugins/table.action.js
@@ -554,8 +554,16 @@ UE.plugins['table'] = function () {
                 getClass = function(list,index,repeat){
                     return list[index] ? list[index] : repeat ? list[index % list.length]: "";
                 };
-            for(var i = 0;i<len;i++){
-                rows[i].className = getClass( classList|| me.options.classList,i,true);
+
+            var index = 0,
+                classIndex = 0;
+
+            if( me.queryCommandState( "inserttitle" ) === -1 ) {
+                index = 1;
+            }
+
+            for(;index<len;index++, classIndex++){
+                rows[index].className = getClass( classList|| me.options.classList,classIndex,true);
             }
         });
         me.addListener("uninterlacetable",function(type,table){
