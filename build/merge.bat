1e18ea90fc63d70b1e0480fdc7f4984c504fbc55
campaign <zhanyi1022@gmail.com>
2013-05-10 17:40:37 +0800

    by zhanyi

--- build/merge.bat
+++
@@ -1,7 +0,0 @@
-java -jar js.jar ueditor_css_all.js ../themes/default/ ../themes/default/css/ueditor.css
-
-java -jar js.jar ueditor_js_all.js ../ ../ueditor.all.js
-java -jar compiler.jar  --js ../ueditor.all.js --js_output_file ../ueditor.all.min.js
-
-
-
