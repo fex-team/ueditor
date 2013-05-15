cfdc167bf669e0de56e5e3135b1a28e12456c598
campaign <zhanyi1022@gmail.com>
2013-05-15 17:59:21 +0800

    by zhanyi

--- _test/ui/uiutils.js
+++ _test/ui/uiutils.js
@@ -6,6 +6,9 @@
  * To change this template use File | Settings | File Templates.
  */
 module( 'ui.uiutils' );
-test( '', function() {
-    equal('','','');
+test( 'parseTmpl', function() {
+    var tmpl = 'aaaa{{text}}';
+    equals(UE.utils.parseTmpl(tmpl,{text:1}),'aaaa1');
+    tmpl = '<ul>{{list}}<li>{{text}}</li>{{/list}}</ul>';
+    equals(UE.utils.parseTmpl(tmpl,{list:[{text:1},{text:2}]}),'<ul><li>1</li><li>2</li></ul>');
 } );
