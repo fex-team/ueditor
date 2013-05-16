c54710dd43b03c5bd4d2fde322e399fb78af4d4a
campaign <zhanyi1022@gmail.com>
2013-05-16 15:47:59 +0800

    by zhanyi

--- _src/ui/uiutils.js
+++ _src/ui/uiutils.js
@@ -1,6 +1,6 @@
 //对core下utils的扩展
 utils.extend(utils,{
-    parseTmpl : function(tmplate,data){
+    parseTmpl : function(tmplate,data,cb){
         var cont = [],reg = /\{\{([^}]+)\}\}/,match;
         function _compile(str,obj){
             while(match = reg.exec(str)){
@@ -11,6 +11,9 @@ utils.extend(utils,{
                         submatch = subreg.exec(str),
                         substr = str.slice(match.index + match[0].length,submatch.index);
                     for(var i= 0,ci;ci=val[i++];){
+                        if(cb && cb(ci,cont)){
+                            continue;
+                        }
                         _compile(substr,ci)
                     }
                     str = str.slice(submatch.index + submatch[0].length)
