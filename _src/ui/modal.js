edc1a69470a128024e9fb74e46e9c6114d2eee8a
许恒 <xuheng@baidu.com>
2013-05-23 17:08:38 +0800

    [add]      by xuheng

--- _src/ui/modal.js
+++ _src/ui/modal.js
@@ -3,20 +3,23 @@ UE.ui.define('modal', {
     tpl: '<div id="help" class="modal hide" tabindex="-1" >' +
         '<div class="modal-header">' +
         '<button type="button" class="close" data-hide="modal">×</button>' +
-        '<h3><%=title%></h3>' +
+        '<h3><%=lang_title%></h3>' +
         '</div>' +
         '<div class="modal-body">' +
         '<iframe height="100%" width="100%" frameborder="0" src="<%=url%>"></iframe>' +
         ' </div>' +
         '<% if(!nook) {%>' +
         '<div class="modal-footer">' +
-        '<button class="btn" data-hide="modal">关闭</button>' +
-        '<button class="btn btn-primary" data-ok="modal">确认</button>' +
+        '<button class="btn" data-hide="modal"><%=lang_hideButton%></button>' +
+        '<button class="btn btn-primary" data-ok="modal"><%=lang_okButton%></button>' +
         '</div>' +
         '<%}%>' +
         '</div>',
     default: {
-        title: "",
+        lang_title: "",
+        lang_hideButton:"",
+        lang_okButton:"",
+
         url: "",
         nook: false,
         backdrop: true,
