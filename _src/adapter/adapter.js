98256bda88823a5c78e0515d555bab3bd82a171f
campaign <zhanyi1022@gmail.com>
2013-05-27 16:29:45 +0800

    by zhanyi

--- _src/adapter/adapter.js
+++ _src/adapter/adapter.js
@@ -6,22 +6,28 @@
 
 (function(){
     var _editorUI = {};
-    UE.registerUI = function(name,fn){
-        utils.each(name.split(/\s+/),function(uiname){
-            _editorUI[uiname] = fn;
-        })
-    };
+    utils.extend(UE,{
+        registerUI : function(name,fn){
+            utils.each(name.split(/\s+/),function(uiname){
+                _editorUI[uiname] = fn;
+            })
+        },
+        getActiveEditor : function(){
+            var ac;
+            utils.each(UE.instants,function(editor){
+                if(editor.selection.isFocus()){
+                    ac = editor;
+                    return false;
+                }
+            });
+            return ac;
+        },
+        getEditor : function(id,options){
 
-    UE.getActiveEditor = function(){
-        var ac;
-        utils.each(UE.instants,function(editor){
-            if(editor.selection.isFocus()){
-                ac = editor;
-                return false;
-            }
-        })
-        return ac;
-    }
+        }
+
+    })
+
 
 
 })();
