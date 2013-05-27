2f33dc43dbc11fe491e9149d6f33e08ae58efee1
campaign <zhanyi1022@gmail.com>
2013-05-27 16:07:06 +0800

    by zhanyi

--- _src/adapter/adapter.js
+++ _src/adapter/adapter.js
@@ -7,8 +7,22 @@
 (function(){
     var _editorUI = {};
     UE.registerUI = function(name,fn){
-        _editorUI[name] = fn;
+        utils.each(name.split(/\s+/),function(uiname){
+            _editorUI[uiname] = fn;
+        })
+    };
+
+    UE.getActiveEditor = function(){
+        var ac;
+        utils.each(UE.instants,function(editor){
+            if(editor.selection.isFocus()){
+                ac = editor;
+                return false;
+            }
+        })
+        return ac;
     }
+
 
 })();
 
