c666b9115d73a8a7bebaa87b3d15ba0688bb5d74
campaign <zhanyi1022@gmail.com>
2013-06-03 14:06:32 +0800

    by zhanyi

--- _src/ui/widget.js
+++ _src/ui/widget.js
@@ -1,4 +1,30 @@
 (function($){
+    //对jquery的扩展
+    $.parseTmpl = function parse(str, data) {
+        var tmpl = 'var __p=[],print=function(){__p.push.apply(__p,arguments);};' + 'with(obj||{}){__p.push(\'' + str.replace(/\\/g, '\\\\').replace(/'/g, "\\'").replace(/<%=([\s\S]+?)%>/g, function(match, code) {
+            return "'," + code.replace(/\\'/g, "'") + ",'";
+        }).replace(/<%([\s\S]+?)%>/g, function(match, code) {
+                return "');" + code.replace(/\\'/g, "'").replace(/[\r\n\t]/g, ' ') + "__p.push('";
+            }).replace(/\r/g, '\\r').replace(/\n/g, '\\n').replace(/\t/g, '\\t') + "');}return __p.join('');";
+        var func = new Function('obj', tmpl);
+        return data ? func(data) : func;
+    };
+    $.extend2 = function (t, s) {
+        var a = arguments,
+            notCover = $.type(a[a.length-1]) == 'boolean' ? a[a.length-1] : false,
+            len = $.type(a[a.length-1]) == 'boolean' ? a.length-1: a.length;
+        for (var i = 1; i < len; i++) {
+            var x = a[i];
+            for (var k in x) {
+                if (!notCover || !t.hasOwnProperty(k)) {
+                    t[k] = x[k];
+                }
+            }
+        }
+        return t;
+    };
+
+
     //所有ui的基类
     var _eventHandler = [];
     var _widget = function(){};
