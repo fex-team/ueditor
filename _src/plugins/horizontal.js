9ce427dac90ced30b578a56fe091f1a90907f8c0
campaign <zhanyi1022@gmail.com>
2013-06-10 02:16:52 +0800

    by zhanyi

--- _src/plugins/horizontal.js
+++ _src/plugins/horizontal.js
@@ -51,11 +51,19 @@ UE.plugins['horizontal'] = function(){
         if(domUtils.isBody(rng.startContainer)){
             var hr = rng.startContainer.childNodes[rng.startOffset -1];
             if(hr && hr.nodeName == 'HR'){
-                var p = this.document.createElement('p');
-                domUtils.fillNode(this.document,p);
-                hr.parentNode.insertBefore(p,hr);
+                var next = hr.nextSibling;
+                if(next){
+                    rng.setStart(next,0)
+                }else if(hr.previousSibling){
+                    rng.setStartAtLast(hr.previousSibling)
+                }else{
+                    var p = this.document.createElement('p');
+                    hr.parentNode.insertBefore(p,hr);
+                    domUtils.fillNode(this.document,p);
+                    rng.setStart(p,0);
+                }
                 domUtils.remove(hr);
-                rng.setStart(p,0).setCursor(false,true);
+                rng.setCursor(false,true);
             }
         }
     })
