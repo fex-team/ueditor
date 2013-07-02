19a496640da8ae70f02a14cfd6cb180fd5af78a2
campaign <zhanyi1022@gmail.com>
2013-07-02 18:34:09 +0800

    by zhanyi

--- _src/plugins/font.js
+++ _src/plugins/font.js
@@ -63,20 +63,19 @@ UE.plugins['font'] = function () {
     }
     function mergeChild(rng,cmdName,value){
         if(needSetChild[cmdName]){
-            var tmpRng = rng.cloneRange();
-            tmpRng.adjustmentBoundary();
-            if(!tmpRng.collapsed && tmpRng.startContainer.nodeType == 1){
-                var start = tmpRng.startContainer.childNodes[tmpRng.startOffset];
+            rng.adjustmentBoundary();
+            if(!rng.collapsed && rng.startContainer.nodeType == 1){
+                var start = rng.startContainer.childNodes[rng.startOffset];
                 if(start && domUtils.isTagNode(start,'span')){
+                    var bk = rng.createBookmark();
                     utils.each(domUtils.getElementsByTagName(start, 'span'), function (span) {
-                        if (!span.parentNode)return;
-
-                        //span.style[needSetChild[cmdName]] =  start.style[needSetChild[cmdName]];
+                        if (!span.parentNode || domUtils.isBookmarkNode(span))return;
                         domUtils.removeStyle(span,needSetChild[cmdName]);
                         if(span.style.cssText.replace(/^\s+$/,'').length == 0){
                             domUtils.remove(span,true)
                         }
                     });
+                    rng.moveToBookmark(bk)
                 }
             }
         }
