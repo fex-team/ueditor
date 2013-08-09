38230ef357af112bfbf5bfa533a5a67e0bace1ff
xuheng <pk3338455>
2013-08-09 12:42:37 +0800

    [add] by xuheng

--- _examples/editor_api.js
+++ _examples/editor_api.js
@@ -57,7 +57,7 @@
             'plugins/fiximgclick.js',
             'plugins/autolink.js',
             'plugins/autoheight.js',
-//            'plugins/autofloat.js',  //依赖UEditor UI,在IE6中，会覆盖掉body的背景图属性
+            'plugins/autofloat.js',  //依赖UEditor UI,在IE6中，会覆盖掉body的背景图属性
             'plugins/puretxtpaste.js',
             'plugins/video.js',
             'plugins/table.core.js',
