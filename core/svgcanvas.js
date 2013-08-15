3ae1161ef452f55340c8b70fee514ce64852469f
许恒 <xuheng@baidu.com>
2013-08-15 14:56:37 +0800

    [add] by xuheng

--- core/svgcanvas.js
+++ core/svgcanvas.js
@@ -1,11 +1,10 @@
 (function(){
-    var Math=UG.Math;
     var current_mode = 'select';
     var current_zoom=1;
     var svgcontent;
     var selectedElements=[];
     var started=false;
-    var root_sctm;
+    var root_sctm,start_x,start_y;
 
     var SvgCanvas = UG.SvgCanvas = function (opt) {
         var me=this;
@@ -31,7 +30,7 @@
         mouseDown: function (evt) {
             root_sctm=svgcontent.canvas.getScreenCTM().inverse();
 
-            var pt = Math.transformPoint(evt.pageX, evt.pageY, root_sctm),
+            var pt = UG.Math.transformPoint(evt.pageX, evt.pageY, root_sctm),
                 mouse_x = pt.x * current_zoom,
                 mouse_y = pt.y * current_zoom;
 
@@ -59,20 +58,72 @@
             }
         },
 
-        mouseMove: function () {
+        mouseMove: function (evt) {
             if (!started) return;
             var selected = selectedElements[0],
-                pt = transformPoint( evt.pageX, evt.pageY, root_sctm ),
+                pt = UG.Math.transformPoint( evt.pageX, evt.pageY, root_sctm ),
                 mouse_x = pt.x * current_zoom,
                 mouse_y = pt.y * current_zoom,
                 shape = selected;
 
             var real_x = x = mouse_x / current_zoom;
             var real_y = y = mouse_y / current_zoom;
+
+            evt.preventDefault();
+
+            switch(current_mode){
+                case "rect":
+                    var square = (current_mode == 'square') || evt.shiftKey,
+                        w = Math.abs(x - start_x),
+                        h = Math.abs(y - start_y),
+                        new_x, new_y;
+
+                    if(square) {
+                        w = h = Math.max(w, h);
+                        new_x = start_x < x ? start_x : start_x - w;
+                        new_y = start_y < y ? start_y : start_y - h;
+                    } else {
+                        new_x = Math.min(start_x,x);
+                        new_y = Math.min(start_y,y);
+                    }
+
+                    shape.attr({
+                        'width': w,
+                        'height': h,
+                        'x': new_x,
+                        'y': new_y
+                    });
+                    break;
+            }
         },
 
-        mouseUp: function () {
+        mouseUp: function (evt) {
+            var pt = UG.Math.transformPoint( evt.pageX, evt.pageY, root_sctm ),
+                mouse_x = pt.x * current_zoom,
+                mouse_y = pt.y * current_zoom,
+                x = mouse_x / current_zoom,
+                y = mouse_y / current_zoom,
+                element = selectedElements[0],
+                keep = false;
 
+            var real_x = x;
+            var real_y = y;
+
+            var useUnit = false; // (curConfig.baseUnit !== 'px');
+            started = false;
+
+            switch (current_mode){
+                case "rect":
+                    var attrs =element.attr(["width", "height"]);
+                    keep = (attrs.width != 0 || attrs.height != 0) || current_mode === "image";
+                    break;
+            }
+
+            if(!keep && element != null){
+
+            }else if(element!=null){
+
+            }
         },
 
         click: function () {
