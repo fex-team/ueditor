97752d038da6ee2ba74a4da38827d30478be513a
yancend <yancend@gmail.com>
2013-08-26 20:03:40 +0800

    testcase

--- _test/plugins/horizontal.js
+++ _test/plugins/horizontal.js
@@ -1,5 +1,4 @@
 module( "plugins.horizontal" );
-
 //normal
 test( 'horizontal', function() {
     var editor = te.obj[0];
@@ -18,23 +17,69 @@ test( 'horizontal', function() {
     },50);
     stop();
 } );
-//TODO 1.2.6
-//test( '在列表中插入分隔线，回车符为p', function() {
-//    var editor = te.obj[0];
-//    var range = te.obj[1];
-//    var body = editor.body;
-//    editor.setContent( '<ol><li><p>top</p></li></ol>' );
-//    range.setStart( body.firstChild.firstChild.firstChild, 1 ).collapse( true ).select();
-//    editor.execCommand( 'horizontal' );
-//
-//    range = editor.selection.getRange();
-//    var p = body.firstChild.firstChild.lastChild;
-//    var space = !ua.browser.ie?'<br>':(ua.browser.ie<9?'&nbsp;':"");
-//    equal( ua.getChildHTML( body ), "<ol><li><p>top</p><hr><p>"+space+"</p></li></ol>", "在列表中插入分隔线，在分隔线后面添加p用于定位" );
-//    if(!ua.browser.opera){
-//        ua.checkResult( range, p, p, 0, 0, true, 'check range' );
-//    }
-//} );
+test( 'horizontal_br', function() {
+    var div = document.body.appendChild(document.createElement('div'));
+    $(div).css('width', '500px').css('height', '500px').css('border', '1px solid #ccc');
+    var editor = new baidu.editor.Editor({'initialContent':'<p>欢迎使用ueditor</p>', 'autoFloatEnabled':false,enterTag:'br'});
+
+    editor.render(div);
+    editor.ready(function () {
+        var range = new baidu.editor.dom.Range(editor.document);
+        editor.setContent( '<ol><li><p>top</p></li></ol>' );
+        range.setStart( editor.body.firstChild.firstChild, 1 ).collapse( true ).select();
+        editor.execCommand( 'horizontal' );
+        range = editor.selection.getRange();
+        var containger = (ua.browser.ie&&ua.browser.ie<9)?editor.body.getElementsByTagName('li')[0]:editor.body.getElementsByTagName('hr')[0].nextSibling;
+        var offset = !ua.browser.ie?1:ua.browser.ie<9?3:0;
+        if(!ua.browser.opera){
+            ua.checkResult( range,containger,containger, offset, offset, true, 'check range' );
+        }
+        ua.manualDeleteFillData(editor.body);
+        equal( ua.getChildHTML( editor.body.firstChild ), "<li><p>top</p><hr><br></li>", "在列表中插入分隔线，在分隔线后面添加p用于定位" );
+        setTimeout(function(){
+            te.dom.push(div);
+            start();
+        },50);
+    });
+    stop();
+} );
+test( 'horizontal-delkeydown', function() {
+    var editor = te.obj[0];
+    var d = editor.document;
+    var range = te.obj[1];
+    var db = editor.body;
+    editor.setContent( '<b><i>top</i></b><p>bottom</p>' );
+    setTimeout(function(){
+        range.setStart( d.getElementsByTagName( 'em' )[0].firstChild, 0 ).setEnd( db.lastChild.firstChild, 5 ).select();
+        editor.execCommand( 'horizontal' );
+        setTimeout(function(){
+            range.setStart( editor.body.lastChild, 0 ).collapse(true).select();
+            editor.fireEvent('delkeydown',{});
+            var spase = ua.browser.ie?'':'<br>';
+            equal( ua.getChildHTML( db ), "<p><strong><em></em></strong></p><p>m"+spase+"</p>", "删除分隔符" );
+            start();
+        },50);
+    },50);
+    stop();
+} );
+
+test( '在列表中插入分隔线，回车符为p', function() {
+    var editor = te.obj[0];
+    var range = te.obj[1];
+    var body = editor.body;
+    editor.setContent( '<ol><li><p>top</p></li></ol>' );
+    range.setStart( body.firstChild.firstChild, 1 ).collapse( true ).select();
+    editor.execCommand( 'horizontal' );
+    range = editor.selection.getRange();
+    var p = body.firstChild.firstChild.lastChild;
+    var offset = (ua.browser.ie&&ua.browser.ie>8)?0:1;
+    if(!ua.browser.opera){
+        ua.checkResult( range, p.firstChild, p.firstChild, offset, offset, true, 'check range' );
+    }
+    ua.manualDeleteFillData(editor.body);
+    equal( ua.getChildHTML( body.firstChild ), "<li><p>top</p><hr><p></p></li>", "在列表中插入分隔线，在分隔线后面添加p用于定位" );
+
+} );
 
 //test( '在列表中插入分隔线，回车符为br', function() {
 //
