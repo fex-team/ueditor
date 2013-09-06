95a86a73fb4eb3eb8817d071bd5d264704fd96c9
hancong03 <121371790@qq.com>
2013-09-06 10:53:41 +0800

    视频修改

--- _src/plugins/video.js
+++ _src/plugins/video.js
@@ -56,10 +56,17 @@ UE.plugins['video'] = function (){
      * @example
      * ```javascript
      *
-     * //editor 是编辑器实例
-     * editor.execCommand( 'insertvideo', {
+     * var videoAttr = {
+     *      //视频地址
+     *      url: 'http://www.youku.com/xxx',
+     *      //视频宽高值， 单位px
+     *      width: 200,
+     *      height: 100
+     * };
      *
-     * } );
+     * //editor 是编辑器实例
+     * //向编辑器插入单个视频
+     * editor.execCommand( 'insertvideo', videoAttr );
      * ```
      */
 
@@ -72,8 +79,24 @@ UE.plugins['video'] = function (){
      * @example
      * ```javascript
      *
+     * var videoAttr1 = {
+     *      //视频地址
+     *      url: 'http://www.youku.com/xxx',
+     *      //视频宽高值， 单位px
+     *      width: 200,
+     *      height: 100
+     * },
+     * videoAttr2 = {
+     *      //视频地址
+     *      url: 'http://www.youku.com/xxx',
+     *      //视频宽高值， 单位px
+     *      width: 200,
+     *      height: 100
+     * }
+     *
      * //editor 是编辑器实例
-     * editor.execCommand( 'insertvideo', [  ] );
+     * //该方法将会向编辑器内插入两个视频
+     * editor.execCommand( 'insertvideo', [ videoAttr1, videoAttr2 ] );
      * ```
      */
 
