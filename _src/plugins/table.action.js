42eb4ffef21e442105971de522f9923d2db68613
Jinqn <459452372>
2013-09-23 14:24:57 +0800

    by Jinqn

--- _src/plugins/table.action.js
+++ _src/plugins/table.action.js
@@ -1796,7 +1796,7 @@ UE.plugins['table'] = function () {
             if (td[0]) {
                 if (flag) {
                     color = (td[0].style.borderColor).replace(/\s/g, "");
-                    if (/(#ffffff)|(rgb\(255,f55,255\))/ig.test(color))
+                    if (/(#ffffff)|(rgb\(255,255,255\))/ig.test(color))
                         domUtils.addClass(node, "noBorderTable")
                 } else {
                     domUtils.removeClasses(node, "noBorderTable")
