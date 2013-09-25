f3edd99d3365cdbd814bc1de7e4e9dbe7bd60afa
Jinqn <459452372>
2013-09-25 19:05:00 +0800

    [fix]by Jinqn 表格重置宽度的bug -- trace3678

--- _src/plugins/table.cmds.js
+++ _src/plugins/table.cmds.js
@@ -870,14 +870,18 @@
     };
 
     function resetTdWidth(table, editor) {
-        var tds = table.getElementsByTagName("td");
+        var tds = domUtils.getElementsByTagName(table,'td th');
         utils.each(tds, function (td) {
             td.removeAttribute("width");
         });
         table.setAttribute('width', getTableWidth(editor, true, getDefaultValue(editor, table)));
+        var tdsWidths = [];
         setTimeout(function () {
             utils.each(tds, function (td) {
-                (td.colSpan == 1) && td.setAttribute("width", td.offsetWidth + "");
+                (td.colSpan == 1) && tdsWidths.push(td.offsetWidth)
+            })
+            utils.each(tds, function (td,i) {
+                (td.colSpan == 1) && td.setAttribute("width", tdsWidths[i] + "");
             })
         }, 0);
     }
