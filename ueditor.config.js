d03cb3415179de4640b769efe53d2af72e754ffa
hancong03 <121371790@qq.com>
2013-09-30 15:08:30 +0800

    路径用列补全 以及 bug修复

--- ueditor.config.js
+++ ueditor.config.js
@@ -409,15 +409,12 @@
     function optimizationPath ( path ) {
 
         var protocol = /^[a-z]+:\/\//.exec( path )[ 0 ],
-            separator = '/',
             tmp = null,
             res = [];
 
         path = path.replace( protocol, "" ).split( "?" )[0].split( "#" )[0];
 
-        separator = path.indexOf( "\\" ) !== -1 ? '\\' : separator;
-
-        path = path.replace( /(\/|\\)\.\1/g, separator).split( /\/|\\/ );
+        path = path.replace( /\\/g, '/').split( /\// );
 
         path[ path.length - 1 ] = "";
 
@@ -425,7 +422,7 @@
 
             if ( ( tmp = path.shift() ) === ".." ) {
                 res.pop();
-            } else {
+            } else if ( tmp !== "." ) {
                 res.push( tmp );
             }
 
