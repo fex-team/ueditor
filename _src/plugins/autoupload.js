7ad899aa8ddd0820266a07c1aaa3e26871e5bb43
Jinqn <459452372>
2013-10-15 15:24:17 +0800

    [add]by Jinqn 更新自动上传插件

--- _src/plugins/autoupload.js
+++ _src/plugins/autoupload.js
@@ -5,11 +5,9 @@
  * @author Jinqn
  * @date 2013-10-14
  */
-UE.plugins['autoupload'] = function() {
+UE.plugin.register('autoupload', function (){
     var me = this;
-    me.setOpt('dropFileEnabled', true);
-
-    function sendAndInsertImage(file, editor){
+    var sendAndInsertImage = function (file, editor) {
         //模拟数据
         var fd = new FormData();
         fd.append(editor.options.imageFieldName || 'upfile', file, file.name || ('blob.' + file.type.substr('image/'.length)));
@@ -28,46 +26,36 @@ UE.plugins['autoupload'] = function() {
             }catch(er){ }
         });
         xhr.send(fd);
-    }
+    };
 
-    if( me.options.dropFileEnabled && window.FormData && window.FileReader) {
-        me.addListener('ready', function(){
-            //插入粘贴板的图片
-            domUtils.on(me.body, 'paste', function (e) {
-                var hasImg = false,
-                    items = e.clipboardData.items, //获取粘贴板文件列表
-                    len = items.length,
-                    file;
-                while (len--){
-                    file = items[len].getAsFile();
-                    if(file && file.size > 0 && /image\/\w+/i.test(file.type) ) {
-                        sendAndInsertImage(file, me);
-                        hasImg = true;
-                    }
+    return {
+        bindEvents:{
+            //插入粘贴板的图片，拖放插入图片
+            'ready':function(e){
+                if(window.FormData && window.FileReader) {
+                    domUtils.on(me.body, 'paste drop', function(e){
+                        var hasImg = false,
+                            items = (e.type == 'paste') ? e.clipboardData.items:e.dataTransfer.files, //获取粘贴板文件列表或者拖放文件列表
+                            len = items.length,
+                            file;
+                        while (len--){
+                            file = items[len];
+                            if(file.getAsFile) file = file.getAsFile();
+                            if(file && file.size > 0 && /image\/\w+/i.test(file.type) ) {
+                                sendAndInsertImage(file, me);
+                                hasImg = true;
+                            }
+                        }
+                        hasImg && e.preventDefault();
+                    });
+                    //取消拖放图片时出现的文字光标位置提示
+                    domUtils.on(me.body, 'dragover', function (e) {
+                        if(e.dataTransfer.types[0] == 'Files') {
+                            e.preventDefault();
+                        }
+                    });
                 }
-                hasImg && e.preventDefault();
-            });
-            //拖拽插入图片
-            domUtils.on(me.body, 'drop', function (e) {
-                var hasImg = false,
-                    items = e.dataTransfer.files, //获取拖放文件列表
-                    len = items.length,
-                    file;
-                while (len--){
-                    file = items[len];
-                    if(file && file.size > 0 && /image\/\w+/i.test(file.type) ) {
-                        sendAndInsertImage(file, me);
-                        hasImg = true;
-                    }
-                }
-                hasImg && e.preventDefault();
-            });
-            //取消拖放图片时出现的文字光标位置提示
-            domUtils.on(me.body, 'dragover', function (e) {
-                if(e.dataTransfer.types[0] == 'Files') {
-                    e.preventDefault();
-                }
-            });
-        });
+            }
+        }
     }
-};
+});
