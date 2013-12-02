fd4b8b9727228835c865be540714fd5d11e5884a
Jinqn <459452372>
2013-12-02 18:15:24 +0800

    by Jinqn

---
+++ js/list.js
@@ -0,0 +1,18 @@
+var docList = [
+    {
+        id: 'use',
+        title: '使用教程',
+        list: [
+            {id:'deploy_tutorial',title:'部署编辑器'},
+            {id:'deploy_asp_server',title:'ASP支持说明'}
+        ]
+    },
+    {
+        id: 'dev',
+        title: '二次开发',
+        list: [
+            {id:'bale_width_grunt',title:'使用grunt打包源代码'},
+            {id:'add_new_button',title:'增加一个简单的按钮'}
+        ]
+    }
+]
