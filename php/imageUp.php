618df5157d2c8653df9faaae874e55bb962c89fa
Jinqn <459452372>
2013-12-11 12:17:39 +0800

    by Jinqn 格式化上传的文件名

--- php/imageUp.php
+++ php/imageUp.php
@@ -31,7 +31,7 @@
         "savePath" => $imgSavePathConfig,
         "maxSize" => 1000, //单位KB
         "allowFiles" => array(".gif", ".png", ".jpg", ".jpeg", ".bmp"),
-        "keepOriginName" => $_POST['keepOriginName']
+        "format" => $_POST['format']
     );
 
     if ( empty( $path ) ) {
