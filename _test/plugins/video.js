8f2fe2c0482c4cff031b20815ae865c88065fae1
Jinqn <459452372@qq.com>
2013-12-18 11:22:21 +0800

    [fix] by Jinqn修改用例

--- _test/plugins/video.js
+++ _test/plugins/video.js
@@ -7,7 +7,32 @@
  */
 module( 'plugins.video' );
 
-test( '视频', function () {
+test( '插入优酷视频', function () {
+    var editor = te.obj[0];
+    var range = te.obj[1];
+    editor.setContent( '<p>hello</p>' );
+    range.setStart(editor.body.firstChild,0).collapse(true).select();
+    var videoObject  ={url: "http://player.youku.com/player.php/Type/Folder/Fid/19275705/Ob/1/sid/XNTU3Mjk4NzQ4/v.swf", width: "500", height: "400", align: "center"}
+    editor.execCommand( 'insertvideo',videoObject);
+    stop();
+    setTimeout(function(){
+        var img = editor.body.getElementsByTagName('img');
+        equal(img.length,1,'插入img');
+        equal(img[0].width,"500");
+        equal(img[0].height,"400");
+        equal(img[0].src,editor.options.UEDITOR_HOME_URL+'themes/default/images/spacer.gif');
+        if(ua.browser.gecko||ua.browser.ie>8){
+            ok(img[0].style.background.indexOf('url(\"'+editor.options.UEDITOR_HOME_URL+'themes/default/images/videologo.gif\")') > -1);
+        }
+        else
+        {
+            ok(img[0].style.background.indexOf("url("+editor.options.UEDITOR_HOME_URL+"themes/default/images/videologo.gif)") > -1);
+        }
+        start();
+    },100);
+} );
+
+test( '插入上传视频', function () {
     var editor = te.obj[0];
     var range = te.obj[1];
     editor.setContent( '<p>hello</p>' );
