20db566eb8b05b97f7ae62991f6c112fc1ac2907
Jinqn <459452372@qq.com>
2014-04-24 10:50:08 +0800

    修改默认上传路径

--- ueditor.config.js
+++ ueditor.config.js
@@ -30,7 +30,7 @@
         UEDITOR_HOME_URL : URL
 
         // 服务器统一请求接口路径
-        ,serverUrl: URL + "net/controller.ashx"
+        ,serverUrl: URL + "net/controller.php"
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义
         , toolbars:[
