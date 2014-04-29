b8997658c7913a16eb64501819a45ae8dae72354
Jinqn <459452372@qq.com>
2014-04-29 14:47:21 +0800

    修复多次切换tab后flash的选文件按钮无效问题

--- dialogs/image/image.js
+++ dialogs/image/image.js
@@ -107,7 +107,7 @@
         if(!id) return;
         var i, bodyId, tabs = $G('tabhead').children;
         for (i = 0; i < tabs.length; i++) {
-            bodyId = tabs[i].getAttribute('data-content-id')
+            bodyId = tabs[i].getAttribute('data-content-id');
             if (bodyId == id) {
                 domUtils.addClass(tabs[i], 'focus');
                 domUtils.addClass($G(bodyId), 'focus');
