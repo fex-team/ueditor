b83c41c3bd89a62c68eeb25f303c9755e1ec20dd
Jinqn <459452372@qq.com>
2014-05-12 15:58:40 +0800

    异步请求后端config

--- _src/core/loadconfig.js
+++ _src/core/loadconfig.js
@@ -2,35 +2,37 @@
 
     UE.Editor.prototype.loadServerConfig = function(){
         var me = this;
-        try{
-            me.options.imageUrl && me.setOpt('serverUrl', me.options.imageUrl.replace(/^(.*[\/]).+([\.].+)$/, '$1controller$2'));
+        setTimeout(function(){
+            try{
+                me.options.imageUrl && me.setOpt('serverUrl', me.options.imageUrl.replace(/^(.*[\/]).+([\.].+)$/, '$1controller$2'));
 
-            var configUrl = me.getActionUrl('config'),
-                isJsonp = utils.isCrossDomainUrl(configUrl);
+                var configUrl = me.getActionUrl('config'),
+                    isJsonp = utils.isCrossDomainUrl(configUrl);
 
-            /* 发出ajax请求 */
-            me._serverConfigLoaded = false;
+                /* 发出ajax请求 */
+                me._serverConfigLoaded = false;
 
-            configUrl && UE.ajax.request(configUrl,{
-                'method': 'GET',
-                'dataType': isJsonp ? 'jsonp':'',
-                'onsuccess':function(r){
-                    try {
-                        var config = isJsonp ? r:eval("("+r.responseText+")");
-                        utils.extend(me.options, config);
-                        me.fireEvent('serverConfigLoaded');
-                        me._serverConfigLoaded = true;
-                    } catch (e) {
-                        console && console.error('后台配置项返回出错!');
+                configUrl && UE.ajax.request(configUrl,{
+                    'method': 'GET',
+                    'dataType': isJsonp ? 'jsonp':'',
+                    'onsuccess':function(r){
+                        try {
+                            var config = isJsonp ? r:eval("("+r.responseText+")");
+                            utils.extend(me.options, config);
+                            me.fireEvent('serverConfigLoaded');
+                            me._serverConfigLoaded = true;
+                        } catch (e) {
+                            console && console.error('后台配置项返回出错!');
+                        }
+                    },
+                    'onerror':function(){
+                        console && console.error('获取后台配置项出错!');
                     }
-                },
-                'onerror':function(){
-                    console && console.error('获取后台配置项出错!');
-                }
-            });
-        } catch(e){
-            console && console.log('获取后台配置项请求出错!');
-        }
+                });
+            } catch(e){
+                console && console.log('获取后台配置项请求出错!');
+            }
+        });
     };
 
     UE.Editor.prototype.isServerConfigLoaded = function(){
