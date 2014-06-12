adaf58b3ecf7d2dd5204424cd97a73554985bed2
Jinqn <459452372@qq.com>
2014-06-12 18:25:46 +0800

    修改文档

--- js/doc.js
+++ js/doc.js
@@ -1,9 +1,15 @@
 $(function(){
-    var path = location.hash.replace(/\?.*$/, '').slice(1).split('-'), cate = path[0], doc = path[1], mdToPath = {};
+    var path = location.hash.replace(/\?.*$/, '').slice(1).split('-'),
+        cate = path[0],
+        doc = path[1],
+        mdToPath = {},
+        pathToMd = {},
+        listArr = []
+        supportHashChange = (window.onhashchange !== undefined);
+
     activeCate = path.length >= 2 ? path[0]:docList[0]['id'];
     activeDoc = path.length >= 2 ? path[1]:docList[0]['list'][0]['id'];
 
-    var listArr = [];
 
     /* 遍历数据对象，生成导航的目录树 */
     $.each(docList, function(ckey, c){
@@ -22,6 +28,7 @@ $(function(){
             }
             $item.append($li);
             mdToPath[v.title] = c.id + '-' + v.id;
+            pathToMd[c.id + '-' + v.id] = v.title;
         });
         if ($item.find('li')) {
             $category.append($item);
@@ -39,13 +46,6 @@ $(function(){
         $category.appendTo('#guidebar>ul');
     });
 
-    /* 设置点击连接，更新导航菜单的active状态 */
-    $('#guidebar .nav .nav>li>a').click(function(){
-        $('#guidebar .nav li').removeClass('active');
-        $(this).parent().addClass('active').parent().parent().addClass('active');
-        updateDocContent($(this).text());
-    });
-
     /* 拉取md文档，并解析插入到页面上 */
     function updateDocContent(activeMd){
         $.get('doc/' + activeMd + '.md',function(s){
@@ -60,17 +60,12 @@ $(function(){
                     .replace(/<code>/g, '<pre>')
                     .replace(/<\/code>/g, '</pre>')
                     .replace(/\<pre\>([^ \s]+)\b/g, '<pre class="prettyprint lang-$1">')
-                    .replace(/\<a href=\"([^\"]*)\.md\"/, function(s, m){
+                    .replace(/\<a href=\"([^\"]*)\.md\"/g, function(s, m){
                         return '<a class="mardwodnlink" href="#' + mdToPath[m] + '"';
                     });
 
             $('#show').html(html + pagebar);
 
-            /* 设置其他markdown的链接 */
-            $('#show .mardwodnlink').click(function(){
-                $('#guidebar .nav .nav>li>a[href=' + $(this).attr('href') + ']').trigger('click');
-                window.scrollTo(0, 1);
-            });
             $('#show pre,#show code').addClass("prettyprint");
             $('#show pre,#show code').each(function(index,node){
 
@@ -81,7 +76,33 @@ $(function(){
                 }
             });
 
+            if (!supportHashChange) {
+                /* 设置其他markdown的链接 */
+                $('#show .mardwodnlink').click(function(){
+                    $('#guidebar .nav .nav>li>a[href=' + $(this).attr('href') + ']').trigger('click');
+                    window.scrollTo(0, 1);
+                });
+            }
+
             prettyPrint();
+        });
+    }
+
+    if (supportHashChange) {
+        /* 设置点击连接，更新导航菜单的active状态 */
+        window.onhashchange = function(){
+            var hash = location.hash;
+            $('#guidebar .nav li').removeClass('active');
+            $('#guidebar .nav .nav>li>a[href=' + hash + ']').parent().addClass('active').parent().parent().addClass('active');
+            updateDocContent(pathToMd[location.hash.substr(1)]);
+        };
+        updateDocContent(pathToMd[location.hash.substr(1)]);
+    } else {
+        /* 设置点击连接，更新导航菜单的active状态 */
+        $('#guidebar .nav .nav>li>a').click(function(){
+            $('#guidebar .nav li').removeClass('active');
+            $(this).parent().addClass('active').parent().parent().addClass('active');
+            updateDocContent($(this).text());
         });
     }
 });
--- js/list.js
+++ js/list.js
@@ -4,142 +4,144 @@ var docList = [
         title: '1. 开始使用',
         list: [
             {
-                id:'get_start',
-                title:'1.1 开始使用ueditor'
+                id:'start',
+                title: '1.1 开始使用ueditor'
             },
             {
                 id:'toolbar',
-                title:'1.2 定制工具栏图标'
-            },
-            {
-                id: 'uparse_use',
-                title: '1.3 编辑内容展示'
+                title: '1.2 定制工具栏图标'
             },
             {
                 id: 'config',
-                title: '1.4 前端配置项说明'
+                title: '1.3 前端配置项说明'
             },
             {
                 id: 'dir',
-                title: '1.5 目录说明'
-            }
-//            {
-//                id: '',
-//                title: '1.5 提交数据说明'
-//            },
-        ]
-    },
-    {
-        id: 'advance',
-        title: '2. 进阶使用',
-        list: [
-            {
-                id: 'upload_video',
-                title: '2.2 上传视频'
+                title: '1.4 目录介绍'
             },
             {
-                id: 'use_patch_file',
-                title: '2.3 如何使用补丁文件'
+                id: 'submit',
+                title: '1.5 提交表单'
+            },
+            {
+                id: 'uparse',
+                title: '1.6 编辑内容展示'
+            },
+            {
+                id: 'patch',
+                title: '1.7 如何使用补丁文件'
             }//,
 //            {
-//                id: '',
-//                title: '2.1 文件上传配置'
+//                id: 'shortkey',
+//                title: '1.8 设置快捷键'
 //            },
 //            {
-//                id: '',
-//                title: '2.1 设置快捷键'
+//                id: 'event',
+//                title: '1.9 监听编辑器事件'
 //            },
 //            {
-//                id: '',
-//                title: '2.2 监听编辑器事件'
-//            },
-//            {
-//                id: '',
-//                title: '2.3 粘贴过滤器(黑白名单)'
-//            },
+//                id: 'filter',
+//                title: '1.10 粘贴过滤器(黑白名单)'
+//            }
         ]
     },
     {
         id: 'server',
-        title: '3. 前后端通信',
+        title: '2. 后端配置',
         list: [
             {
-                id:'request_specification',
-                title: '3.1 后端请求规范'
+                id:'deploy',
+                title: '2.1 后端部署说明'
             },
             {
-                id:'server_config',
-                title: '3.2 后端配置项说明'
+                id:'php',
+                title: '2.2 PHP 使用说明'
             },
             {
-                id:'format_filepath',
-                title: '3.3 格式化上传文件名'
+                id:'asp',
+                title: '2.3 ASP 使用说明'
             },
             {
-                id:'server_param',
-                title: '3.4 自定义请求参数'
+                id:'net',
+                title: '2.4 ASP.NET 使用说明'
             },
             {
-                id:'crossdomain',
-                title: '3.5 跨域支持说明'
+                id:'jsp',
+                title: '2.5 JSP 使用说明'
             },
             {
-                id:'server_asp',
-                title: '3.6 ASP支持说明'
+                id:'config',
+                title: '2.6 后端配置项说明'
             },
             {
-                id:'server_net',
-                title: '3.7 ASP.NET 使用说明'
+                id:'path',
+                title: '2.7 上传路径配置'
+            },
+            {
+                id: 'video',
+                title: '2.8 上传视频'
             }
         ]
     },
     {
         id: 'dev',
-        title: '4. 二次开发',
+        title: '3. 二次开发',
         list: [
             {
                 id: 'developer',
-                title: '4.1 如何进行二次开发'
+                title: '3.1 如何进行二次开发'
             },
             {
                 id: 'bale_width_grunt',
-                title: '4.2 使用grunt打包源代码'
+                title: '3.2 使用grunt打包源代码'
+            },
+            {
+                id:'request_specification',
+                title: '3.3 后端请求规范'
+            },
+            {
+                id:'serverparam',
+                title: '3.4 自定义请求参数'
+            },
+            {
+                id:'crossdomain',
+                title: '3.5 跨域支持说明'
             }//,
 //            {
 //                id: '',
-//                title: '3.3 修改编辑区域样式'
+//                title: '3.6 修改编辑区域样式'
 //            },
 //            {
 //                id: '',
-//                title: '3.4 输入输出过滤规则'
+//                title: '3.7 输入输出过滤规则'
 //            },
         ]
     },
     {
         id: 'api',
-        title: '5. 常用API列表',
+        title: '4. 常用API列表',
         list: [
             {
                 id: 'common',
-                title: '5.1 常用API'
+                title: '4.1 常用API'
             }
         ]
     },
     {
         id: 'qa',
-        title: '6. 常见问题',
+        title: '5. 常见问题',
         list: [
             {
                 id:'ready',
-                title:'6.1 实例创建后为什么直接执行接口报错'
+                title:'5.1 实例创建后为什么直接执行接口报错'
             },
             {
                 id:'customurl',
-                title:'6.2 如何自定义请求地址'
+                title:'5.2 如何自定义请求地址'
             },
             {
                 id:'allowDivToP',
-                title:'6.3 如何阻止div变成p标签'
+                title:'5.3 如何阻止div变成p标签'
             }
 
         ]
