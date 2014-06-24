a094fbdccc7e8385a2125e92a0afa21f93c2234d
Jinqn <459452372@qq.com>
2014-06-24 12:58:04 +0800

    修改上传

--- dialogs/attachment/attachment.js
+++ dialogs/attachment/attachment.js
@@ -485,7 +485,7 @@
 
             uploader.on('uploadBeforeSend', function (file, data, header) {
                 //这里可以通过data对象添加POST参数
-                if (uploader.request('predict-runtime-type') == 'html5') {
+                if (actionUrl.toLowerCase().indexOf('jsp') != -1) {
                     header['X_Requested_With'] = 'XMLHttpRequest';
                 }
             });
