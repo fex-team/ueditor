442ff41b56502a41b94d6edd00d3ebb4259a01ce
Jinqn <459452372@qq.com>
2014-08-25 17:19:49 +0800

    修复创建编辑器后，快速销毁编辑器后的报错

--- _src/adapter/message.js
+++ _src/adapter/message.js
@@ -53,7 +53,7 @@ UE.registerUI('message', function(editor) {
     });
 
     function updateHolderPos(){
-        if (holder || me.ui) return;
+        if (!holder || !me.ui) return;
         var toolbarbox = me.ui.getDom('toolbarbox');
         if (toolbarbox) {
             holder.style.top = toolbarbox.offsetHeight + 3 + 'px';
