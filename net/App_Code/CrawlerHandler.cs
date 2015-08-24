cfa764db1756ba36a0278913be4053a2d631390f
zhangzhenyang <zhangzhenyang@baidu.com>
2015-08-25 01:21:19 +0800

    增加远程抓取图片时对私有地址的过滤

--- net/App_Code/CrawlerHandler.cs
+++ net/App_Code/CrawlerHandler.cs
@@ -56,6 +56,11 @@ public class Crawler
 
     public Crawler Fetch()
     {
+        if (!IsExternalIPAddress(this.SourceUrl))
+        {
+            State = "INVALID_URL";
+            return this;
+        }
         var request = HttpWebRequest.Create(this.SourceUrl) as HttpWebRequest;
         using (var response = request.GetResponse() as HttpWebResponse)
         {
@@ -99,5 +104,61 @@ public class Crawler
             }
             return this;
         }
+    }
+
+    private bool IsExternalIPAddress(string url)
+    {
+        var uri = new Uri(url);
+        switch (uri.HostNameType)
+        {
+            case UriHostNameType.Dns:
+                var ipHostEntry = Dns.GetHostEntry(uri.DnsSafeHost);
+                foreach (IPAddress ipAddress in ipHostEntry.AddressList)
+                {
+                    byte[] ipBytes = ipAddress.GetAddressBytes();
+                    if (ipAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    {
+                        if (!IsPrivateIP(ipAddress))
+                        {
+                            return true;
+                        }
+                    }
+                }
+                break;
+
+            case UriHostNameType.IPv4:
+                return !IsPrivateIP(IPAddress.Parse(uri.DnsSafeHost));
+        }
+        return false;
+    }
+
+    private bool IsPrivateIP(IPAddress myIPAddress)
+    {
+        if (IPAddress.IsLoopback(myIPAddress)) return true;
+        if (myIPAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+        {
+            byte[] ipBytes = myIPAddress.GetAddressBytes();
+            // 10.0.0.0/24 
+            if (ipBytes[0] == 10)
+            {
+                return true;
+            }
+            // 172.16.0.0/16
+            else if (ipBytes[0] == 172 && ipBytes[1] == 16)
+            {
+                return true;
+            }
+            // 192.168.0.0/16
+            else if (ipBytes[0] == 192 && ipBytes[1] == 168)
+            {
+                return true;
+            }
+            // 169.254.0.0/16
+            else if (ipBytes[0] == 169 && ipBytes[1] == 254)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
