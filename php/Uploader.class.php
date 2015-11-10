f3bf5c439b83fc21f6a7ca7ee1e9ced596b9bfea
birdol <admin@birdol.com>
2015-11-10 17:17:52 +0800

    Update Uploader.class.php
    强迫症，错别字。

--- php/Uploader.class.php
+++ php/Uploader.class.php
@@ -273,7 +273,7 @@ class Uploader
         $format = str_replace("{ss}", $d[6], $format);
         $format = str_replace("{time}", $t, $format);
 
-        //过滤文件名的非法自负,并替换文件名
+        //过滤文件名的非法字符,并替换文件名
         $oriName = substr($this->oriName, 0, strrpos($this->oriName, '.'));
         $oriName = preg_replace("/[\|\?\"\<\>\/\*\\\\]+/", '', $oriName);
         $format = str_replace("{filename}", $oriName, $format);
