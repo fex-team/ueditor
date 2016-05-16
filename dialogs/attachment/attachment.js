ed5b7ef8240d01d34d5cfb49689c22aea6aae7d0
sjshare <minixyx@qq.com>
2016-05-16 13:48:00 +0800

    修改attachment.js本地文件上传
    修改本地文件上传，当上传文件超出大小或格式不允许时，点击移除后文件总数及总大小出错bug。

--- dialogs/attachment/attachment.js
+++ dialogs/attachment/attachment.js
@@ -439,8 +439,10 @@
             }
 
             uploader.on('fileQueued', function (file) {
-                fileCount++;
-                fileSize += file.size;
+                if (file.ext && acceptExtensions.indexOf(file.ext.toLowerCase()) != -1 && file.size <= fileMaxSize) {
+                    fileCount++;
+                    fileSize += file.size;
+                }
 
                 if (fileCount === 1) {
                     $placeHolder.addClass('element-invisible');
@@ -451,8 +453,10 @@
             });
 
             uploader.on('fileDequeued', function (file) {
-                fileCount--;
-                fileSize -= file.size;
+                if (file.ext && acceptExtensions.indexOf(file.ext.toLowerCase()) != -1 && file.size <= fileMaxSize) {
+                    fileCount--;
+                    fileSize -= file.size;
+                }
 
                 removeFile(file);
                 updateTotalProgress();
