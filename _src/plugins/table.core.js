cb4ca9765fd91dc4336fd9e9d5275af6ef91d47c
dengxi <dengxi@baidu.com>
2017-06-14 16:34:58 +0800

    注释掉关于表头合并的特殊处理逻辑

--- _src/plugins/table.core.js
+++ _src/plugins/table.core.js
@@ -856,21 +856,23 @@
           this.indexTable[range.beginRowIndex][range.beginColIndex].cellIndex
         );
 
-      if (
-        leftTopCell.tagName == "TH" &&
-        range.endRowIndex !== range.beginRowIndex
-      ) {
-        var index = this.indexTable,
-          info = this.getCellInfo(leftTopCell);
-        leftTopCell = this.getCell(1, index[1][info.colIndex].cellIndex);
-        range = this.getCellsRange(
-          leftTopCell,
-          this.getCell(
-            index[this.rowsNum - 1][info.colIndex].rowIndex,
-            index[this.rowsNum - 1][info.colIndex].cellIndex
-          )
-        );
-      }
+      // 这段关于行表头或者列表头的特殊处理会导致表头合并范围错误
+      // 为什么有这段代码的原因未明，暂且注释掉
+      // if (
+      //   leftTopCell.tagName == "TH" &&
+      //   range.endRowIndex !== range.beginRowIndex
+      // ) {
+      //   var index = this.indexTable,
+      //     info = this.getCellInfo(leftTopCell);
+      //   leftTopCell = this.getCell(1, index[1][info.colIndex].cellIndex);
+      //   range = this.getCellsRange(
+      //     leftTopCell,
+      //     this.getCell(
+      //       index[this.rowsNum - 1][info.colIndex].rowIndex,
+      //       index[this.rowsNum - 1][info.colIndex].cellIndex
+      //     )
+      //   );
+      // }
 
       // 删除剩余的Cells
       var cells = this.getCells(range);
