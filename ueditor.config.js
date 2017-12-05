c5907343382370f42c8dbfce01a69572fcd59ee1
马金花儿 <o.o@mug.dog>
2017-12-05 14:31:59 +0800

    Update ueditor.config.js

--- ueditor.config.js
+++ ueditor.config.js
@@ -362,7 +362,7 @@
 		//output xss过滤
 		,outputXssFilter: true
 		// xss过滤白名单 名单来源: https://raw.githubusercontent.com/leizongmin/js-xss/master/lib/default.js
-		,whitList: {
+		,whiteList: {
 			a:      ['target', 'href', 'title', 'class', 'style'],
 			abbr:   ['title', 'class', 'style'],
 			address: ['class', 'style'],
