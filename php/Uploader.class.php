655f269aebe51de885f6420f6d5fce36aa6021c6
jianzhiyao <739319867@qq.com>
2018-09-01 09:54:58 +0800

    10000000000 某些环境超过 PHP 最大整形
    ## 系统相关
    ### PHP_VERSION
    7.0.12
    ### OS
    Windows x64
    
    ## 测试代码
    ```php
    <?php
    var_dump([
        '10000000000' => 10000000000,
        'PHP_INT_MAX' => PHP_INT_MAX,
        'PHP_VERSION' => PHP_VERSION
    ]);
    exit;
    ```
    
    ## 结果输出
    ```
    array (size=3)
      '10000000000' => float 10000000000
      'PHP_INT_MAX' => int 2147483647
      'PHP_VERSION' => string '7.0.12' (length=6)
    ```
    
    10000000000 已超过 当前环境下 PHP 最大整形

--- php/Uploader.class.php
+++ php/Uploader.class.php
@@ -290,7 +290,7 @@ class Uploader
         $format = str_replace("{filename}", $oriName, $format);
 
         //替换随机字符串
-        $randNum = rand(1, 10000000000) . rand(1, 10000000000);
+        $randNum = rand(1, PHP_INT_MAX) . rand(1, PHP_INT_MAX);
         if (preg_match("/\{rand\:([\d]*)\}/i", $format, $matches)) {
             $format = preg_replace("/\{rand\:[\d]*\}/i", substr($randNum, 0, $matches[1]), $format);
         }
